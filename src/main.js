6bdf6cf0d5da6e440159710fcb7a6ca59d05a301
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2018-02-06 09:37:25 +0800

    small fix

--- src/main.js
+++ src/main.js
@@ -34,7 +34,7 @@ Vue.filter('formatFullUrl', Filter.formatFullUrl)
 Vue.filter('capitalize', Filter.capitalize)
 
 router.beforeEach((to, frm, next) => {
-  if (to.path === '/protect' || store.state.auth.secret_code === process.env.SECRET_CODE) {
+  if (/^\/(protect|_oauth|confirm)/.test(to.path) || store.state.auth.secret_code === process.env.SECRET_CODE) {
     next()
   } else {
     next('/protect')
