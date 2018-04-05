49a04c4e4b2abe144700624a01ca50c504021684
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2018-04-05 19:08:40 +0800

    implement admin global_stats

--- src/views/admin/components/global_stats.ctrl.js
+++ src/views/admin/components/global_stats.ctrl.js
@@ -1,4 +1,6 @@
+import _ from 'lodash'
 import moment from 'moment'
+import { Filter } from '@/helper'
 import AdminService from '@/services/admin'
 
 export default {
@@ -19,18 +21,18 @@ export default {
         { name: 'Artist Signups', value: 0 },
         { name: 'Brand Signups', value: 0 },
         { name: 'Label Signups', value: 0 },
-        { name: 'Listeners', value: 0 },
         { name: 'Uploads', value: 0 },
         { name: 'Downloads', value: 0 },
         { name: 'Plays', value: 0 },
         { name: 'Merch Uploads', value: 0 },
         { name: 'Merch Sales', value: 0 },
-        { name: 'Repost Requests', value: 0 },
+        { name: 'Reposted Albums', value: 0 },
+        { name: 'Reposted Merch', value: 0 },
         { name: '# of Donations', value: 0 },
-        { name: 'Revenue made from Donations', value: 0 },
+        { name: 'Revenue made from Donations', value: '0.00' },
         { name: 'Playlists Created', value: 0 },
-        { name: 'Top 10 popular genres', value: 0 },
-        { name: 'Blocked Users (view users', value: 0 },
+        { name: 'Top 5 downloaded genres', value: '' },
+        { name: 'Top 5 listened genres', value: '' },
         { name: 'Cancelled Accounts', value: 0 }
       ],
       per_page_options: [10, { text: "All", value: -1 }],
@@ -71,19 +73,19 @@ export default {
           { name: 'Artist Signups', value: stats.signup_artist_users },
           { name: 'Brand Signups', value: stats.signup_brand_users },
           { name: 'Label Signups', value: stats.signup_label_users },
-          { name: 'Listeners', value: 0 },
           { name: 'Uploads', value: stats.uploaded_albums },
           { name: 'Downloads', value: stats.downloaded_albums },
           { name: 'Plays', value: stats.played_albums },
           { name: 'Merch Uploads', value: stats.uploaded_products },
           { name: 'Merch Sales', value: stats.sold_products },
-          { name: 'Repost Requests', value: 0 },
-          { name: '# of Donations', value: 0 },
-          { name: 'Revenue made from Donations', value: 0 },
+          { name: 'Reposted Albums', value: stats.reposted_albums },
+          { name: 'Reposted Merch', value: stats.reposted_products },
+          { name: '# of Donations', value: stats.donations_count },
+          { name: 'Revenue made from Donations', value: Filter.formatNumber(stats.donations_revenue) },
           { name: 'Playlists Created', value: stats.created_playlists },
-          { name: 'Top 10 popular genres', value: 0 },
-          { name: 'Blocked Users (view users)', value: 0 },
-          { name: 'Cancelled Accounts', value: 0 }
+          { name: 'Top 5 downloaded genres', value: _.map(stats.top_5_downloaded_genres, 'name').join(', ') },
+          { name: 'Top 5 listened genres', value:  _.map(stats.top_5_played_genres, 'name').join(', ') },
+          { name: 'Cancelled Accounts', value: stats.cancelled_accounts }
         ]
       }).catch(e => {
         console.log(e)
