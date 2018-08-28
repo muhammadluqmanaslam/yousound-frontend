04f4cf0be84f88504e22b11d2d522ded6be2387f
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2018-08-28 10:00:33 +0800

    link old browser icons to browser sites

--- src/views/home/browser.vue
+++ src/views/home/browser.vue
@@ -3,11 +3,11 @@
     <h2>Update Your Browser</h2>
     <p>We only support the latest browsers, and there’s plently to choose from.</p>
     <ul>
-      <li><div class="chrome"></div></li>
-      <li><div class="firefox"></div></li>
-      <li><div class="brave"></div></li>
-      <li><div class="opera"></div></li>
-      <li><div class="yandex"></div></li>
+      <li><a class="chrome" href="https://www.google.com/chrome/"></a></li>
+      <li><a class="firefox" href="https://www.mozilla.org/en-US/firefox/"></a></li>
+      <li><a class="brave" href="https://brave.com/download/"></a></li>
+      <li><a class="opera" href="https://www.opera.com/"></a></li>
+      <li><a class="yandex" href="https://browser.yandex.com/"></a></li>
     </ul>
   </div>
 </template>
