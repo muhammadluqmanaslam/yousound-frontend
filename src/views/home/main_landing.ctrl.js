a160fbd6b2428539c4d8fbd48a0de5432b68107d
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2019-02-28 01:42:16 +0800

    implement main_landing page without slick component

--- src/views/home/main_landing.ctrl.js
+++ src/views/home/main_landing.ctrl.js
@@ -1,29 +1,24 @@
-import Slick from 'vue-slick';
-
-import '@/../node_modules/slick-carousel/slick/slick.css';
-import '@/../node_modules/slick-carousel/slick/slick-theme.css';
-
 export default {
-  components: {
-    Slick
-  },
+  components: {},
 
   data () {
     return {
-      slickOptions: {
-        slidesToShow: 1,
-        arrows: false,
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 3000,
-      }
+      windowWidth: 0,
+      activeSlide: 1,
     }
   },
 
   computed: {
+    transform () {
+      return `translate3d(-${this.windowWidth * (this.activeSlide - 1)}px, 0px, 0px)`;
+    }
   },
 
   created () {
+    window.addEventListener('resize', this.handleResize)
+    this.windowWidth = window.innerWidth
+    console.log('main_landing created', this.windowWidth)
+
     if (this.$store) {
       if (this.$store.state.auth.user) {
         this.$router.push({ path: '/discover' })
@@ -33,60 +28,15 @@ export default {
     }
   },
 
+  destroyed () {
+    window.removeEventListener('resize', this.handleResize)
+  },
+
   methods: {
-    next() {
-      console.log('next')
-      this.$refs.slick.next()
-    },
-    prev() {
-      console.log('prev')
-      this.$refs.slick.prev()
-    },
-    reInit() {
-      console.log('reInit')
-      // Helpful if you have to deal with v-for to update dynamic lists
-      this.$nextTick(() => {
-        this.$refs.slick.reSlick();
-      });
-    },
-    handleAfterChange(event, slick, currentSlide) {
-      // console.log('handleAfterChange', slick, currentSlide, this.$refs.slick)
-      // console.log('handleAfterChange', currentSlide, this.$refs.slick.getOption('autoplay'))
-      if (currentSlide === 3) {
-        // console.log('autoplay pause')
-        this.slickOptions.autoplay = false
-        this.$refs.slick.setOption('autoplay', false, true)
-      }
-    },
-    // handleBeforeChange(event, slick, currentSlide, nextSlide) {
-    //     console.log('handleBeforeChange', event, slick, currentSlide, nextSlide);
-    // },
-    // handleBreakpoint(event, slick, breakpoint) {
-    //     console.log('handleBreakpoint', event, slick, breakpoint);
-    // },
-    // handleDestroy(event, slick) {
-    //     console.log('handleDestroy', event, slick);
-    // },
-    // handleEdge(event, slick, direction) {
-    //     console.log('handleEdge', event, slick, direction);
-    // },
-    // handleInit(event, slick) {
-    //     console.log('handleInit', event, slick);
-    // },
-    // handleReInit(event, slick) {
-    //     console.log('handleReInit', event, slick);
-    // },
-    // handleSetPosition(event, slick) {
-    //     console.log('handleSetPosition', event, slick);
-    // },
-    // handleSwipe(event, slick, direction) {
-    //     console.log('handleSwipe', event, slick, direction);
-    // },
-    // handleLazyLoaded(event, slick, image, imageSource) {
-    //     console.log('handleLazyLoaded', event, slick, image, imageSource);
-    // },
-    // handleLazeLoadError(event, slick, image, imageSource) {
-    //     console.log('handleLazeLoadError', event, slick, image, imageSource);
-    // },
+
+    handleResize () {
+      this.windowWidth = window.innerWidth
+      console.log('main_landing handleResize', this.windowWidth)
+    }
   }
 }
--- src/views/home/main_landing.vue
+++ src/views/home/main_landing.vue
@@ -1,49 +1,77 @@
 <template>
   <div class="page main-landing-page">
-    <slick
-      ref="slick"
-      :options="slickOptions"
-      @afterChange="handleAfterChange"
-    >
-      <div>
-        <div class="slide">
-          <div><img src="/static/images/img_landing_albums.jpg"></div>
+    <div class="slides-container" :style="{transform: transform}">
+      <div class="slide slide-1">
+        <div class="slide__content">
+          <div>
+            <img src="/static/images/img_landing_albums.jpg">
+          </div>
           <p>
             Free streaming.<br>
             Free downloading.<br>
             No ads.
           </p>
         </div>
+        <ul class="dots">
+          <li @click="activeSlide = 1" class="active"></li>
+          <li @click="activeSlide = 2"></li>
+          <li @click="activeSlide = 3"></li>
+          <li @click="activeSlide = 4"></li>
+        </ul>
       </div>
 
-      <div>
-        <div class="slide">
-          <div><img src="/static/images/img_landing_products.jpg"></div>
+      <div class="slide slide-2">
+        <div class="slide__content">
+          <div>
+            <img src="/static/images/img_landing_products.jpg">
+          </div>
           <p>
             Share content &<br>
             earn money.<br>
             Shop.
           </p>
         </div>
+        <ul class="dots">
+          <li @click="activeSlide = 1"></li>
+          <li @click="activeSlide = 2" class="active"></li>
+          <li @click="activeSlide = 3"></li>
+          <li @click="activeSlide = 4"></li>
+        </ul>
       </div>
 
-      <div>
-        <div class="slide">
-          <div><img src="/static/images/img_landing_videos.jpg"></div>
+      <div class="slide slide-3">
+        <div class="slide__content">
+          <div>
+            <img src="/static/images/img_landing_videos.jpg">
+          </div>
           <p>
             Live video chat<br>
             with artists &<br>
             friends.
           </p>
         </div>
+        <ul class="dots">
+          <li @click="activeSlide = 1"></li>
+          <li @click="activeSlide = 2"></li>
+          <li @click="activeSlide = 3" class="active"></li>
+          <li @click="activeSlide = 4"></li>
+        </ul>
       </div>
 
-      <div>
-        <div class="slide">
-          <div><img class="promote" src="/static/images/img_landing_promote.jpg"></div>
+      <div class="slide slide-4">
+        <div class="slide__content">
+          <div>
+            <img src="/static/images/img_landing_promote.jpg">
+          </div>
         </div>
+        <ul class="dots">
+          <li @click="activeSlide = 1"></li>
+          <li @click="activeSlide = 2"></li>
+          <li @click="activeSlide = 3"></li>
+          <li @click="activeSlide = 4" class="active"></li>
+        </ul>
       </div>
-    </slick>
+    </div>
   </div>
 </template>
 
