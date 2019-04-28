60737dbc994c300f093fd06a40ef2a4c1d6805e3
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2019-04-28 12:41:12 +0800

    make logo equal in reset_password page

--- src/views/auth/forgot_password.vue
+++ src/views/auth/forgot_password.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-layout row wrap class="page auth-page auth-forgot-page">
+  <v-layout row wrap>
     <v-flex xs12 sm6 offset-sm3 md6 offset-md3 lg8 offset-lg2 xl8 offset-xl2 transition="slide-x-reverse-transition">
-      <div class="page-header">
+      <div class="page auth-page auth-login-header">
         <v-flex xs12 text-xs-center>
           <img class="logo" src="/static/images/nav_logo_primary.png">
         </v-flex>
@@ -9,7 +9,7 @@
           <h4 class="forgot-title">Forgot your password?</h4>
         </v-flex>
       </div>
-      <div class="page-content">
+      <div class="page auth-page auth-forgot-page">
         <form v-on:submit.prevent="submit()">
           <v-flex xs12 text-xs-center>
             <!-- <div class="form-group" :class="{'has-error': errors.has('username') }" >
--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -21,22 +21,18 @@
           <v-flex xs12 text-xs-center>
             <input class="password" type="password" v-model="user.password" placeholder="Password">
           </v-flex>
-          <v-flex xs12>
-            <v-flex xs12>
-              <v-flex xs12>
-                <v-layout row wrap>
-                  <v-flex sm6 xs12>
-                    <p class="regular-checkbox">
-                      <input type="checkbox" id="checkbox" v-model="remember">
-                      <label for="checkbox">Remember me</label>
-                    </p>
-                  </v-flex>
-                  <v-flex sm6 xs12 text-xs-right text-sm-right>
-                    <router-link to="/forgot">Forgot password?</router-link>
-                  </v-flex>
-                </v-layout>
+          <v-flex xs12 px-2>
+            <v-layout row wrap>
+              <v-flex sm6 xs12>
+                <p class="regular-checkbox">
+                  <input type="checkbox" id="checkbox" v-model="remember">
+                  <label for="checkbox">Remember me</label>
+                </p>
               </v-flex>
-            </v-flex>
+              <v-flex sm6 xs12 text-xs-right text-sm-right>
+                <router-link to="/forgot">Forgot password?</router-link>
+              </v-flex>
+            </v-layout>
           </v-flex>
           <v-flex xs12 text-xs-center>
             <v-btn block round dark type="submit" class="login-button">Sign in</v-btn>  
@@ -44,7 +40,9 @@
         </form>
         <v-flex xs12 text-xs-center>
           <v-layout row class="or-divider">
-            <v-flex xs5><hr class="divider"></v-flex><v-flex xs2><p class="or">OR</p></v-flex><v-flex xs5><hr class="divider"></v-flex>
+            <v-flex xs5><hr class="divider"></v-flex>
+            <v-flex xs2><p class="or">OR</p></v-flex>
+            <v-flex xs5><hr class="divider"></v-flex>
           </v-layout>
         </v-flex>
         <v-flex xs12 text-xs-center>
