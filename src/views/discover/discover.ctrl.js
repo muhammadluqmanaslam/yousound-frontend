a159425910a77bbae2f7933cc379f54714e439e4
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2019-05-06 23:25:25 -0700

    Add staggered loading

--- src/views/discover/discover.ctrl.js
+++ src/views/discover/discover.ctrl.js
@@ -30,7 +30,7 @@ export default {
       hover_on_genre_tooltip: false,
       page_index: 1,
       total_pages: 1,
-      items_per_page: 1 * 20,
+      items_per_page: 1 * 10,
       genres: [],
       selected_genre: null,
       categories: [],
@@ -214,6 +214,10 @@ export default {
       }
       this.$nextTick(() => {
         this.loadFeeds(this.activeTab)
+        this.loadMore()
+        this.loadMore()
+        this.loadMore()
+        this.loadMore()
       })
     }
   },
