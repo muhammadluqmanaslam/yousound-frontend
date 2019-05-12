3a9279a29e1268960e189a5023dd27fcf5c8b7e1
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2019-05-12 20:57:03 +0800

    add asterisk to required field in product add/edit page

--- src/views/sell/add_product.vue
+++ src/views/sell/add_product.vue
@@ -23,7 +23,7 @@
           </v-flex>
           <v-flex xs12 product-section>
             <v-flex xs12 form-group>
-              <label class="control-label">Product Name</label>
+              <label class="control-label">Product Name<label class="required">*</label></label>
               <input type="text" class="form-control" v-model="product.name">
             </v-flex>
             <v-flex xs12 form-group>
@@ -39,7 +39,7 @@
               </v-radio-group>
             </v-flex>
             <v-flex xs12 form-group>
-              <label class="control-label">Category</label>
+              <label class="control-label">Category<label class="required">*</label></label>
               <v-select
                 v-bind:items="product_categories"
                 v-model="product.category"
@@ -76,19 +76,20 @@
             </v-flex>
             <v-layout row product-option-content form-group v-for="(variant, index) in product.variants" :key="index">
               <v-flex xs12 sm4>
-                <label class="control-label">Option Name</label>
+                <label class="control-label">Name<label class="required">*</label></label>
                 <input type="text" class="form-control" v-model="variant.name" :disabled="isDigitalProduct">
               </v-flex>
               <v-flex xs12 sm4 v-if="!isDigitalProduct">
-                <label class="control-label">In Stock</label>
+                <label class="control-label"># In Stock<label class="required">*</label></label>
                 <input type="text" class="form-control" v-model="variant.quantity">              
               </v-flex>
               <v-flex xs12 sm4>
-                <label class="control-label">Price</label>
+                <label class="control-label">Price<label class="required">*</label></label>
                 <input type="text" class="form-control" v-model="variant.price">
               </v-flex>
               <v-icon class="clear-btn" @click="deleteVariant(index)">clear</v-icon>
             </v-layout>
+            <p class="d-inline-flex grey--text lighten-2">e.g. "Large - Black" "100" "$30.00"</p>
           </v-flex>
         </v-flex>
 
@@ -220,7 +221,7 @@
             <div class="vertical-middle-div">
               <v-layout row>
                 <v-flex xs12 sm4>
-                  <label class="control-label">Image 1</label>
+                  <label class="control-label">Image 1<label class="required">*</label></label>
                   <div class="product-image-section">
                     <!-- <img class="product-image" id="product_image1"  v-if="product.image1"/> -->
                     <div class="product-image" id="product_image1"  v-if="product.image1"></div>
