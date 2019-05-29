7ca3658bf3badcee2ce305b7e0697350ac59c9db
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2019-05-30 03:47:07 +0800

    enable uppercase in username, automatically converted in downcase on api side

--- src/views/auth/register_by_listener.vue
+++ src/views/auth/register_by_listener.vue
@@ -27,7 +27,7 @@
               name="username"
               placeholder="Username"
               v-model="user.username"
-              v-validate="{ required: true, min: 3, max: 20, regex: /^[a-z0-9_.]+$/ }"
+              v-validate="{ required: true, min: 3, max: 20, regex: /^[A-Za-z0-9_.]+$/ }"
               @blur="onBlur"
               class="username"
             >
