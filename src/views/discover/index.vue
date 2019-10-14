895573f01a219601fd738bb92371df810634fb15
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2019-10-14 13:50:32 -0700

    front end edits

--- src/views/discover/index.vue
+++ src/views/discover/index.vue
@@ -3,7 +3,7 @@
     <div class="d-flex">
       <div class="page-left">
         <div class="tab-container">
-          <h2 class="page-title">Display</h2>
+          <h2 class="page-title">Discover</h2>
           <ul class="pr-3">
             <li
               v-for="tab in tabs"
