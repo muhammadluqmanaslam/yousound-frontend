ff484e192eced2be73d012a8c8cfcd2139cd3bf7
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2020-02-12 10:02:50 +0800

    donate to track owner in download modal

--- src/components/downloadmodal.vue
+++ src/components/downloadmodal.vue
@@ -8,26 +8,26 @@
     <v-layout row wrap class="popup-section">
       <v-flex xs12 class="profile-section">
         <!-- <div class="avatar-image" style="background-image: url('/static/images/user1.jpg');"></div> -->
-        <router-link :to="'/' + item.user.slug">
+        <router-link :to="'/' + trackUser.slug">
           <div
             class="avatar-image"
-            :style="{'background-image': 'url(' + item.user.avatar.thumb.url + ')'}"
+            :style="{'background-image': 'url(' + trackUser.avatar.thumb.url + ')'}"
           ></div>
         </router-link>
-        <router-link :to="'/' + item.user.slug">
+        <router-link :to="'/' + trackUser.slug">
           <label class="user-name">
-            {{ item.user.display_name }}
+            {{ trackUser.display_name }}
             <v-icon
-              v-if="item.user.user_type == 'artist'"
+              v-if="trackUser.user_type == 'artist'"
               class="user-status"
-              :class="{'online': item.user.status == 'active'}"
+              :class="{'online': trackUser.status == 'active'}"
             >fa-check-circle</v-icon>
           </label>
         </router-link>
         <v-btn
-          v-if="item.user.id!=$store.state.auth.user.id"
+          v-if="trackUser.id != currentUser.id"
           class="follow-btn"
-          :class="{'follow': !item.user.is_following, 'following': item.user.is_following}"
+          :class="{'follow': !trackUser.is_following, 'following': trackUser.is_following}"
           @mouseenter="buttonHover = true"
           @mouseleave="buttonHover = false"
           @click.native="followUser()"
@@ -103,15 +103,23 @@
 
     data () {
       return {
-        donate_amount: null,
+        donate_amount: 0,
         showPaymentModal: false,
         buttonHover: false
       }
     },
 
     computed: {
+      currentUser () {
+        return this.$store.state.auth.user
+      },
+
+      trackUser () {
+        return this.track ? this.track.user : this.item.user
+      },
+
       followButtonText () {
-        if (this.item.user.is_following) {
+        if (this.trackUser.is_following) {
           return this.buttonHover ? 'Unfollow' : 'Following'
         }
         return 'Follow'
@@ -131,21 +139,21 @@
       },
 
       followUser () {
-        if (this.item.user.is_following) {
-          UserService.unfollowUser(this.item.user.id).then(response => {
-            this.$store.dispatch('error/showSuccessToast', ['You just unfollowed ' + this.item.user.display_name])
-            this.item.user.is_following = false
-            // this.$store.dispatch('player/setUpdatedUser', this.item.user)
-            this.$root.$emit(MyEvents.USER_FOLLOW, this.item.user.id, false)
+        if (this.trackUser.is_following) {
+          UserService.unfollowUser(this.trackUser.id).then(response => {
+            this.$store.dispatch('error/showSuccessToast', ['You just unfollowed ' + this.trackUser.display_name])
+            this.track.user.is_following = false
+            // this.$store.dispatch('player/setUpdatedUser', this.trackUser)
+            this.$root.$emit(MyEvents.USER_FOLLOW, this.trackUser.id, false)
           }).catch(e => {
             this.$store.dispatch('error/showErrorToast', e.body.errors || [e.body])
           })
         } else {
-          UserService.followUser(this.item.user.id).then(response => {
-            this.$store.dispatch('error/showSuccessToast', ['You just followed ' + this.item.user.display_name])
-            this.item.user.is_following = true
-            // this.$store.dispatch('player/setUpdatedUser', this.item.user)
-            this.$root.$emit(MyEvents.USER_FOLLOW, this.item.user.id, true)
+          UserService.followUser(this.trackUser.id).then(response => {
+            this.$store.dispatch('error/showSuccessToast', ['You just followed ' + this.trackUser.display_name])
+            this.track.user.is_following = true
+            // this.$store.dispatch('player/setUpdatedUser', this.trackUser)
+            this.$root.$emit(MyEvents.USER_FOLLOW, this.trackUser.id, true)
           }).catch(e => {
             this.$store.dispatch('error/showErrorToast', e.body.errors || [e.body])
           })
@@ -207,7 +215,7 @@
         if (token) {
           params['payment_token'] = token.id
         }
-        UserService.donateMoney(this.item.user.slug, params).then(response => {
+        UserService.donateMoney(this.trackUser.slug, params).then(response => {
           this.$store.dispatch('error/showSuccessToast', [`You sent user $${Filter.formatNumber(this.donate_amount_by_cent)}`])
           AuthService.setUser(response.body)
           this.downloadItem()
