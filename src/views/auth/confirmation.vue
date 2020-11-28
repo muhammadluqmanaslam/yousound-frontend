732c89499170ac9a0f30251cb5ee4eeadfaaa3f5
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2020-11-28 14:36:42 -0500

    add some text in confirmation page

--- src/views/auth/confirmation.vue
+++ src/views/auth/confirmation.vue
@@ -12,9 +12,11 @@
         </div>
         <div class="page-content">
           <v-flex xs12 text-xs-center>
-            <!-- <p>We've sent a confirmation link to your e-mail<br><a>{{ email }}</a></p>
-            <p v-if="user_type!=0">Your account is currently being reviewed for verification, please allow up to 5 business days. You will receive an email shortly. You currently have a listener account.</p> -->
-            <!-- <hr/> -->
+            <p>
+              If you don't see the email in your main inbox<br />check your
+              other folders like 'promotions'
+            </p>
+            <!-- <p v-if="user_type!=0">Your account is currently being reviewed for verification, please allow up to 5 business days. You will receive an email shortly. You currently have a listener account.</p> -->
             <v-btn round dark @click.native="sendConfirmEmail()"
               >Resend Confirmation E-mail</v-btn
             >
