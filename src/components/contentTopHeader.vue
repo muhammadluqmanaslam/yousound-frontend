528ae18d7e226d059738f250c9b6046653047cac
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2021-10-06 22:49:51 +0100

    implement top header ancestor

---
+++ src/components/contentTopHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="top-menu">
+    <div class="tab-container">
+        <!-- ancestoral element to all top headers -->
+        <slot name="topHeader"></slot>
+    </div>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style src="@/../static/styles/topHeader.scss" lang="scss" scoped>
+</style>
