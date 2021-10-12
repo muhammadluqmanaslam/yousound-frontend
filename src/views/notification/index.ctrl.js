c8b07724bb5952082b3754467abdb2461b87ca9a
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2021-10-12 22:49:36 +0100

    round up notification update

--- src/views/notification/index.ctrl.js
+++ src/views/notification/index.ctrl.js
@@ -1,111 +1,49 @@
-import _ from 'lodash'
-// import { mapGetters } from 'vuex'
-import ActivityService from '@/services/activity'
-import activityItem from '@/components/activityitem'
+import contentTopHeader from '@/components/contentTopHeader'
+import Activity from '@/views/activity'
+import Messages from '@/views/messages'
 
 export default {
   components: {
-    activityItem,
+    contentTopHeader,
+    Activity,
+    Messages,
   },
-
   data() {
     return {
-      active_tab: 'any',
+      currentTab: 'activity',
       tabs: [
-        { id: 'any', title: 'Everything' },
-        { id: 'repost', title: 'Reposts' },
-        { id: 'comment', title: 'Commented' },
-        { id: 'follow', title: 'Followed' },
+        { id: 'activity', title: 'Activity' },
+        { id: 'messages', title: 'Messages' },
       ],
-      page_index: 1,
-      total_pages: 1,
-      items_per_page: 8 * 1,
-      activities: [],
-      isPageReady: false,
     }
   },
-
-  computed: {
-    currentUser() {
-      return this.$store.state.auth.user
-    },
-  },
-
+  computed: {},
   methods: {
-    // filterSelected (index) {
-    //   $('#filter_selector .btn__content').html(this.tabs[index].name + '<i class="material-icons icon icon--right theme--dark">keyboard_arrow_down</i>')
-    //   this.active_tab = this.tabs[index].id
-    // },
-
     isActiveTab(tab) {
-      return this.active_tab == tab
+      console.log(tab)
+      return this.currentTab === tab
     },
-
-    loadActivities() {
-      this.$store.dispatch('error/showLoadingActivity', true)
-      const params = {
-        page: this.page_index,
-        per_page: this.items_per_page,
-        action_types: this.active_tab,
-      }
-      ActivityService.getActivities(params)
-        .then((response) => {
-          this.activities = this.activities.concat(response.body.activities)
-          this.page_index = response.body.pagination.current_page
-          this.total_pages = response.body.pagination.total_pages
-          this.$store.dispatch('error/showLoadingActivity', false)
-          this.isPageReady = true
-        })
-        .catch((e) => {
-          this.$store.dispatch('error/showLoadingActivity', false)
-          this.isPageReady = true
-          this.$store.dispatch(
-            'error/showErrorToast',
-            e.body.errors || [e.body]
-          )
-        })
-    },
-
-    loadMore() {
-      this.page_index += 1
-      this.loadActivities()
-    },
-
-    onTab(tab) {
+    setTab(tab) {
+      this.currentTab = tab
       this.$router.push({
         path: this.$route.path,
         hash: tab,
       })
     },
-
-    setTab(tab) {
-      if (!tab) {
-        tab = 'any'
-      }
-
-      this.active_tab = tab
-      this.page_index = 1
-      this.total_pages = 1
-      this.activities = []
-      this.$store.dispatch('navigator/goNextState', {
-        page: 'activity',
-        tab: tab,
-      })
-      this.$nextTick(() => {
-        this.loadActivities(this.activeTab, 1)
-      })
-    },
   },
-
   watch: {
     $route(toPath, fromPath) {
+      console.log(this.active_tab)
+
+      console.log(toPath)
+      console.log(fromPath)
       const tab = toPath.hash.substr(1)
       this.setTab(tab)
     },
   },
 
   created() {
-    const tab = this.$route.hash.substr(1)
+    const tab = this.$route.hash.substr(1) || 'activity'
     this.setTab(tab)
   },
 }
--- src/views/notification/index.vue
+++ src/views/notification/index.vue
@@ -1,7 +1,30 @@
 <template>
   <div class="page notification-page mx-5">
-    <h1>Notifications</h1>
+    <content-top-header>
+      <template slot="topHeader">
+        <ul>
+          <li
+            v-for="tab in tabs"
+            :key="tab.id"
+            :href="`#${tab.id}`"
+            :class="{ active: isActiveTab(tab.id) }"
+          >
+            <label @click="setTab(tab.id)">{{ tab.title }}</label>
+          </li>
+        </ul>
+      </template>
+    </content-top-header>
+
+      <div class="d-flex">
+        <div class="page-content">
+          <Activity v-if="currentTab === 'activity'" />
+          <Messages v-else-if="currentTab === 'messages'" />
+        </div>
+      </div>
+
+
   </div>
 </template>
 
-// <script type="text/javascript" src="./index.ctrl.js"></script>
+<script type="text/javascript" src="./index.ctrl.js"></script>
+<style src="../../../static/styles/notifications.scss" lang="scss" scoped>
