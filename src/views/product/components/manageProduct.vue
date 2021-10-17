026f165cb4a946b7b34a4b56016831cc5a2704d5
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2021-10-18 00:05:50 +0100

    manage product progress

---
+++ src/views/product/components/manageProduct.vue
@@ -0,0 +1,19 @@
+<template>
+  <div row wrap class="page manage-album-page">
+    <div class="d-flex">
+      <div class="page-content" v-if="currentUser && isPageReady">
+        <div>
+            <product-item
+            v-for="(product, index) in setProductFilter()"
+            :index="index"
+            :key="index"
+            :product="product"
+            :deleteItem="openProductDeleteConfirmDialog"
+            ></product-item>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript" src="./manageProduct.ctrl.js"></script>
