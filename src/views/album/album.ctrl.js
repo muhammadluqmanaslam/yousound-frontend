324c0f8ee8c02cd36e929e2a266971e81dd68883
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2021-10-21 17:41:48 +0100

    reverse canva gradient background

--- src/views/album/album.ctrl.js
+++ src/views/album/album.ctrl.js
@@ -175,10 +175,7 @@ export default {
 
   created() {
     // this.$store.dispatch('navigator/setCurrentState', { page: 'upload', tab: '' })
-    this.$store.dispatch('navigator/goNextState', {
-      page: 'album/show',
-      tab: '',
-    })
+    this.$store.dispatch('navigator/goNextState', { page: 'album', tab: '' })
     // console.log('current', this.$store.state.navigator.current)
     // console.log('last', this.$store.getters['navigator/last'])
 
@@ -200,7 +197,6 @@ export default {
     }),
 
     loadData() {
-      this.$store.dispatch('error/showLoadingActivity', true)
       const vm = this
       this.slug = this.$route.params.slug
       this.comments = []
@@ -218,7 +214,6 @@ export default {
       ])
         .then((values) => {
           this.album = values[0].body
-          console.log(this.album)
           // Vue.set(this, "album", values[0].body)
           // for (let index in this.album.tracks) {
           //   this.buttonHover.push(false)
@@ -236,19 +231,19 @@ export default {
           }
           this.$emit('updateHead')
 
-          // setTimeout(function () {
-          //   vm.changeBackground()
-          //   var height = $('#album_info_page').height() + 230
-          //   var screen_height = $(window).height()
-          //   if (height > screen_height) {
-          //     height += 50
-          //   } else {
-          //     height = screen_height
-          //   }
-          //   var canvas = document.getElementById('canvas')
-          //   canvas.height = height
-          //   // $('#back_image').css("cssText", "height: " + height + "px !important;")
-          // }, 200)
+          setTimeout(function () {
+            vm.changeBackground()
+            var height = $('#album_info_page').height() + 230
+            var screen_height = $(window).height()
+            if (height > screen_height) {
+              height += 50
+            } else {
+              height = screen_height
+            }
+            var canvas = document.getElementById('canvas')
+            canvas.height = height
+            // $('#back_image').css("cssText", "height: " + height + "px !important;")
+          }, 200)
 
           if (this.comments_subscription) {
             this.comments_subscription.unsubscribe()
@@ -308,7 +303,6 @@ export default {
             }
           )
 
-          this.$store.dispatch('error/showLoadingActivity', false)
           this.isPageReady = true
         })
         .catch((reason) => {
@@ -648,24 +642,23 @@ export default {
   },
 
   mounted() {
-    // const vm = this
-    // $(window)
-    //   .resize(function () {
-    //     var height = $('#album_info_page').height() + 230
-    //     var screen_height = $(window).height()
-    //     if (height > screen_height) {
-    //       height += 50
-    //     } else {
-    //       height = screen_height
-    //     }
-    //     var canvas = document.getElementById('canvas')
-    //     if (canvas) {
-    //       $('#canvas').css('cssText', 'height: ' + height + 'px !important;')
-    //     }
-    //     $('#back_image').css('cssText', 'height: ' + height + 'px !important;')
-    //   })
-    //   .trigger('resize')
-
+    const vm = this
+    $(window)
+      .resize(function () {
+        var height = $('#album_info_page').height() + 230
+        var screen_height = $(window).height()
+        if (height > screen_height) {
+          height += 50
+        } else {
+          height = screen_height
+        }
+        var canvas = document.getElementById('canvas')
+        if (canvas) {
+          $('#canvas').css('cssText', 'height: ' + height + 'px !important;')
+        }
+        $('#back_image').css('cssText', 'height: ' + height + 'px !important;')
+      })
+      .trigger('resize')
   },
 
   updated() {},
