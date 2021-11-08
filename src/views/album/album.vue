6c35189549a0c2368403e8d52685696ab3118f3b
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2021-11-08 16:35:51 +0100

    remove redundant header

--- src/views/album/album.vue
+++ src/views/album/album.vue
@@ -396,8 +396,6 @@
 
     <v-layout class="secondary-album-details">
       <v-flex xs8 album-comment-page v-if="currentUser">
-        <h1>Comment Feature</h1>
-
         <comments :item="album" :comments="comments" />
       </v-flex>
 
