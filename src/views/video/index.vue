00a9497d6b118e937bb3543f8a1ca76817ee130f
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2021-11-15 17:22:22 +0100

    fic key loop error

--- src/views/video/index.vue
+++ src/views/video/index.vue
@@ -6,9 +6,9 @@
           <v-tabs-bar>
             <v-tabs-item
               v-model="selectedTab"
-              v-for="(genre, i) in available_genres"
-              :key="i"
-              :href="'#tab-' + i"
+              v-for="(genre, idx) in available_genres"
+              :key="idx"
+              :href="'#tab-' + idx"
             >
               <v-chip class="text-capitalize" @click.native="setTab(genre.id)">
                 {{ genre.name.toLowerCase() }}
@@ -49,10 +49,10 @@
       <div class="page-content" v-if="currentUser">
         <v-layout row wrap>
           <!-- <v-flex xs4 v-for="(video, i) in videos" :key="i" class="video-container top-3"> -->
-          <v-flex xs4 v-for="(video, i) in videos.slice(0,3)" :key="i" class="video-container top-3">
+          <v-flex xs4 v-for="(video, i) in videos.slice(0,3)" :key="video.name" class="video-container top-3">
             <video-box :hoverOverlay="false" :item="video" />
           </v-flex>
-          <v-flex xs3 v-for="(video, i) in videos.slice(3,videos.length)" :key="i" class="card-container">
+          <v-flex xs3 v-for="(video, ii) in videos.slice(3,videos.length)" :key="video.name" class="card-container">
             <video-box :hoverOverlay="false" :item="video" />
           </v-flex>
         </v-layout>
