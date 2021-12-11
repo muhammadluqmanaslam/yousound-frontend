ebbc6800893e0f726548e3ce34474c12dfe3d48d
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2021-12-12 00:00:39 +0100

    get paid update

--- src/views/getPaidToShare/index.vue
+++ src/views/getPaidToShare/index.vue
@@ -4,12 +4,11 @@
         <v-layout wrap align-center justify-center>
             <v-flex xs5 layer-flex flex-text>
                 <h1>Get paid  <br> to share.</h1>
-                <h3 class="mt-2">Upgrade to remove ads & accept Repost <br /> Requests.</h3>
+                <!-- <h3 class="mt-2">Upgrade to remove ads & accept Repost <br /> Requests.</h3> -->
 
                 <span>
-                <v-btn class="black white--text mt-4">
-                    <v-icon class="mr-2">south</v-icon>
-                    <span>Upgrade</span>
+                <v-btn depressed class="choose__btn __rounded white--text mt-4" to="/plans">
+                    <span>Choose Plan</span>
                 </v-btn>
                 </span>
             </v-flex>
@@ -21,48 +20,42 @@
             </v-flex>
         </v-layout>
 
-        <h1 class="text-center my-5">Choose the right plan for you</h1>
 
-        <v-layout wrap align-center justify-space-between pt-5>
-            <v-flex
-              v-for="(plan, i) in plans"
-              :key="i" 
-              xs4
-              plan-layer
-              flex-text
-            >
-            <div class="plan-card" :class="plan.value">
-                <h2>{{ plan.title }}</h2>
+
+        <v-layout wrap align-center justify-center>
+            <v-flex xs6 layer-flex flex-img>
+                <img
+                  width="60%"
+                  src="/static/images/re_whyBg.svg"
+                />
+            </v-flex>
+            <v-flex xs5 layer-flex flex-text>
+                <h2>Why?</h2>
+
                 <ul>
-                  <li
-                    v-for="(b, j) in plan.benefits"
-                    :key="j"
-                  >
-                    <div>
-                      <v-icon>check</v-icon>
-                    </div>
-
-                    <div>
-                      <span>
-                        {{ b }}
-                      </span>
-                    </div>
-                  </li>
+                  <li>Sharing is valuable</li>
+                  <li>Content is experienced when it’s shared </li>
+                  <li>You are part of the sharing process</li>
                 </ul>
-
-                <v-spacer></v-spacer>
-
-                <div class="plan-card_price">
-                  <span class="__currency">$</span>
-                  <span class="__price">{{ plan.price }}</span>
-                  <span class="__duration">/month</span>
-                </div>
-
-                <v-btn depressed class="width100 mx-0 choose__btn">Choose</v-btn>
-            </div>
-
             </v-flex>
         </v-layout>
+
+        <h1 class="mt-5">How?</h1>
+        <div>
+          Verified artists & brands can pay you to share their albums, videos & products to your followers with Repost Requests.   Artists and brands will send you a Direct Message and attach their content for you to share. Before the message is sent artists & brands will pay the repost price you set. 
+          <br />
+          <br />
+          When you receive the message you can view the content & decide if you want to repost or deny. Accepted requests will stay on your feed for 7 days and you will receive payment in 2 days. You can charge up to <b>5k per repost</b> with the <b>Plus</b> subscription plan, or up to <b>100k per repost</b> with a <b>Pro</b> subscription plan.
+        </div>
+
+        <br>
+        <br>
+
+        <h2>Go ad-free.  Support Creators.  Get Paid.</h2>
+
+        <v-btn depressed class="choose__btn __rounded white--text mt-4" to="/plans">
+            <span>Choose Plan</span>
+        </v-btn>
     </v-container>
 
 
@@ -95,11 +88,6 @@
         line-height: 1;
       }
       .__duration {}
-    }
-
-    .choose__btn {
-      background-color: #5D5FEF !important;
-      color: #ffffff;
     }
 
     li {
