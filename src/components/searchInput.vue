e98c0f6fec2d921240cd11287e57f9eee3ceb437
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2021-12-27 18:20:21 +0100

    intro search as own component

---
+++ src/components/searchInput.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="search-input">
+    <transition name="slide-fade">
+        <div class="global-search-box">
+        <v-icon>search</v-icon>
+        <input
+            v-model="keyword"
+            class="search-field"
+            type="search"
+            id="search"
+            placeholder="Search artist, song or keyword"
+            @keyup.enter="goToSearch()"
+        />
+        </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      keyword: '',
+    }
+  },
+  methods: {
+    goToSearch() {
+      const keyword = this.keyword
+      this.$router.push({ path: '/search', query: { q: keyword } })
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
