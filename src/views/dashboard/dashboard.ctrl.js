cbfe4717fe2412441d9908234a1f95972a0550b5
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2021-12-31 21:53:54 +0100

    progress on dashboard

--- src/views/dashboard/dashboard.ctrl.js
+++ src/views/dashboard/dashboard.ctrl.js
@@ -1,15 +1,20 @@
 import contentTopHeader from '@/components/contentTopHeader'
 import dashboardNav from '@/components/dashboardnav'
+import chart from 'vue-apexcharts'
 
 export default {
   components: {
     contentTopHeader,
     dashboardNav,
+    chart,
   },
   data() {
     return {
       activeTab: 'overview',
       activeInnerTab: 'earnings',
+      activeChart: '',
+      selectedInnerTab: {},
+      selectedChart: {},
       daysFilter: 7,
       tabs: [
         { id: 'overview', title: 'Overview'},
@@ -23,7 +28,46 @@ export default {
         {
           parent: 'overview',
           tabs: [
-            { id: 'earnings', title: 'Earnings', value: 1000, size: 'custom' },
+            {
+              id: 'earnings',
+              title: 'Earnings',
+              value: 1000,
+              size: 'custom',
+              breakdown: [
+                {
+                  title: 'All',
+                  value: 'all',
+                  series: [{
+                    name: 'series-1',
+                    data: [0, 10, 45, 50, 80, 60, 70, 45, 10, 0, 0, 0],
+                  }],
+                },
+                {
+                  title: 'Sales',
+                  value: 'sales',
+                  series: [{
+                    name: 'series-1',
+                    data: [30, 40, 45, 50, 49, 60, 70, 81, 0, 0, 0, 0],
+                  }],
+                },
+                {
+                  title: 'Donations',
+                  value: 'donations',
+                  series: [{
+                    name: 'series-1',
+                    data: [30, 40, 45, 50, 70, 81, 49, 60, 0, 0],
+                  }],
+                },
+                {
+                  title: 'Repost Request',
+                  value: 'repostRequest',
+                  series: [{
+                    name: 'series-1',
+                    data: [30, 40, 0, 50, 49, 60, 70, 81, 0, 0, 0, 0],
+                  }],
+                },
+              ],
+            },
             { id: 'followers', title: 'Followers', value: 2000, size: 'custom' },
             { id: 'shares', title: 'Shares', value: 3000, size: 'custom' },
             { id: 'comments', title: 'Comments', value: 4000, size: 'custom' },
@@ -35,11 +79,31 @@ export default {
         },
         {
           parent: 'music',
+          header: 'Plays',
           tabs: [
-            { id: 'social', title: 'Social' },
-            { id: 'albums', title: 'Albums' },
-            { id: 'videos', title: 'Videos' },
-            { id: 'products', title: 'Products' }
+            {
+              id: 'listening',
+              title: 'Listening Now',
+              value: 1000,
+              size: 'custom',
+              breakdown: [
+                {
+                  title: 'All',
+                  value: 'all',
+                  series: [{
+                    name: 'series-1',
+                    data: [0, 30, 45, 70, 80, 60, 30, 45, 10, 0, 0, 0],
+                  }],
+                },
+              ],
+            },
+            { id: 'followers', title: 'Followers', value: 2000, size: 'custom' },
+            { id: 'shares', title: 'Shares', value: 3000, size: 'custom' },
+            { id: 'comments', title: 'Comments', value: 4000, size: 'custom' },
+            { id: 'reposts', title: 'Reposts', value: 5000, size: 'custom' },
+            { id: 'subscriptionRevenue', title: 'Subscription Revenue', value: 6000, size: 4 },
+            { id: 'subscribersList', title: 'Subscribers List', value: 7000, size: 4 },
+            { id: 'adRevenue', title: 'Ad Revenue', value: 8000, size: 4 },
           ],
         },
       ],
@@ -54,12 +118,192 @@ export default {
           title: 'Last 7 Days', value: 7,
         },
       ],
+      chartOptions: {
+        chart: {
+          id: 'own-id',
+          zoom: {
+            enabled: false,
+          },
+        },
+        xaxis: {
+          type: 'category',
+          categories: [
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Aug",
+            "Sep",
+            "Oct",
+            "Nov",
+            "Dec",
+          ],
+        },
+      },
+      pieSeries: [44, 55, 41, 17, 15],
+      pieChartOptions: {
+        chart: {
+          type: 'donut',
+        },
+        labels: ["Country A", "Country B", "Country C", "Country D", "Country E"],
+        responsive: [{
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 100,
+            },
+            legend: {
+              position: 'bottom',
+            },
+          },
+        }],
+        dataLabels: {
+          enabled: false,
+        },
+        legend: {
+          show: !false,
+          position: 'bottom',
+        },
+        plotOptions: {
+          pie: {
+            customScale: 0.7,
+            donut: {
+              size: '80%',
+            },
+          },
+        },
+      },
+      headers: [
+        {
+          text: 'External Traffic Source',
+          align: 'left',
+          sortable: false,
+          value: 'name',
+        },
+        {
+          text: 'Visits',
+          align: 'left',
+          sortable: false,
+          value: 'name',
+        },
+        {
+          text: 'New Visitors',
+          align: 'left',
+          sortable: false,
+          value: 'name',
+        },
+        {
+          text: 'Album Page Visited',
+          align: 'left',
+          sortable: false,
+          value: 'name',
+        },
+      ],
+      desserts: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: '1%'
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: '1%'
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: '7%'
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: '8%'
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          iron: '16%'
+        },
+        {
+          name: 'Jelly bean',
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+          iron: '0%'
+        },
+        {
+          name: 'Lollipop',
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+          iron: '2%'
+        },
+        {
+          name: 'Honeycomb',
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          iron: '45%'
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: '22%'
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: '6%'
+        }
+      ],
     }
   },
 
   computed: {
     currentUser() {
       return this.$store.state.auth.user
+    },
+    showChartHeader() {
+      return ['overview', 'products']
+    },
+  },
+
+  watch: {
+    activeTab(val) {
+      // find and select first index of breakdown on menu change
+      const inner = this.innerTabs.find((inner) => inner.parent === val)
+      this.selectedInnerTab = inner
+      this.selectedChart = inner.tabs[0].breakdown[0]
+      this.activeInnerTab = inner.tabs[0].id
+      this.activeChart = inner.tabs[0].breakdown[0].value
     },
   },
 
@@ -68,29 +312,42 @@ export default {
       return this.activeTab === tab
     },
     isActiveInnerTab(tab) {
-      // this.activeInnerTab = tab
-      // const innerTab = this.setInnerTab()
-
-      // console.log(this.activeInnerTab === innerTab.id);
-
       return this.activeInnerTab === tab
     },
-    setInnerTab() {
-      const inner = this.innerTabs.find((inner) => inner.parent === this.activeTab)
-      return inner.tabs
+    isActiveChart(tab) {
+      return this.activeChart === tab
     },
     setInnerMenuTabs(filter) {
       const selected = this.tabs.find((f) => f.id === filter)
       this.activeTab = selected.id
-
-      // switch (filter) {
-      //   case 'dashboard':
-      //     return this.dashboardTabs
-      //   case 'sales':
-      //     return this.albumsMenuTabs
-      //   default:
-      //     []
-      // }
     },
+    getInnerTab() {
+      const inner = this.innerTabs.find((inner) => inner.parent === this.activeTab)
+      return inner.tabs
+    },
+    setActiveChart(tab) {
+      this.activeChart = tab.value
+      this.selectedChart = tab
+    },
+    getChartDetails(action) {
+      this.activeInnerTab = action.id
+      this.selectedInnerTab = action
+
+      // For Menus that do not have further inner tab selection
+      if (this.showChartHeader.indexOf(this.activeTab) < 0) {
+        this.setActiveChart(action.breakdown[0])
+      }
+    },
+  },
+
+  created() {
+    // device inital chart
+    // change default from activeInnerTab: '' above
+    const innerTab = this.getInnerTab()
+    const initSelection = innerTab.find((f) => f.id === this.activeInnerTab)
+    this.getChartDetails(initSelection)
+
+    this.selectedChart = this.selectedInnerTab.breakdown[0]
+    this.activeChart = this.selectedInnerTab.breakdown[0].value
   },
 }
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -35,13 +35,13 @@
         <v-container fluid grid-list-lg>
             <v-layout row wrap class="inner-tab-actions">
                 <v-flex 
-                    v-for="(action, i) in setInnerTab()" 
+                    v-for="(action, i) in getInnerTab()" 
                     :key="i"
                     xs12
                     :sm="`sm${action.size}`"
                     class="inner-tab-action"
                     :class="[{'inner-tab-active': isActiveInnerTab(action.id)}, action.size == 'custom' ? 'custom-lg5' : action.size ? `sm${action.size}` : '']"
-                    @click="activeInnerTab = action.id"
+                    @click="getChartDetails(action)"
                 >
                     <div class="action_content">
                         <div>{{ action.value }}</div>
@@ -49,6 +49,128 @@
                     </div>
                 </v-flex>
             </v-layout>
+        </v-container>
+
+        <v-container fluid>
+            <div class="graph-chart chart-wrapper">
+                <div class="chart-header">
+                    <content-top-header v-if="showChartHeader.indexOf(activeTab) > -1" absolute height="20" class="__inner pl-0">
+                        <template slot="topHeader">
+                            <ul class="chart-header-tabs width100">
+                                <li
+                                    v-for="tab in selectedInnerTab.breakdown"
+                                    :key="tab.id"
+                                    :href="`#${tab.value}`"
+                                    class="nav-li"
+                                    :class="[{ 'active d-nav-active': isActiveChart(tab.value) }, `nav-${tab.id}`]"
+                                >
+                                <label class="nav-label black--text" @click="setActiveChart(tab)">
+                                    {{ tab.title }}
+                                </label>
+                                </li>
+
+                                <v-spacer></v-spacer>
+                                <li>
+                                    <h2>${{ selectedInnerTab.value | formatNumberWithComma }}</h2>
+                                </li>
+                            </ul>
+                        </template>
+                    </content-top-header>
+                    <h2 v-else>{{ selectedInnerTab.header }}</h2>
+                </div>
+
+                <!-- selectedInnerTab: {{ selectedInnerTab }} <br> <br>
+                activeInnerTab: {{ activeInnerTab }} <br>
+                selectedChart: {{ selectedChart }} <br>
+                activeChart: {{ activeChart }} <br> -->
+
+                <div id="chart" :class="[activeInnerTab ? `${activeInnerTab}-chart` : '']">
+                    <chart type="area" height="250" width="100%" :options="chartOptions" :series="selectedChart.series"></chart>
+                </div>
+            </div>
+
+            <div class="col-summary-section">
+                <v-container fluid grid-list-lg>
+                    <v-layout row wrap  justify-space-between>
+                        <v-flex xs3 col-summary>
+                            <div class="col-summary-frame">
+                                <div class="col-summary-frame-header">
+                                    <h4>Top Albums</h4>
+                                </div>
+                                <div class="col-summary-frame-body">
+                                    <div v-for="(item, i) in 7" :key="i" class="col-listing __square">
+                                        <div class="_thumbnail"></div>
+                                        <div class="_title">Album Name</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </v-flex>
+
+                        <v-flex xs3 col-summary>
+                            <div class="col-summary-frame">
+                                <div class="col-summary-frame-header">
+                                    <h4>Plays by Country</h4>
+                                </div>
+                                <div class="col-summary-frame-body">
+                                    <div id="chart" class="pie-chart">
+                                        <chart type="donut" height="500" :options="pieChartOptions" :series="pieSeries"></chart>
+
+                                        {{ chartOptions.color }}
+                                    </div>
+                                </div>
+                            </div>
+                        </v-flex>
+
+                        
+                        <v-flex xs3 col-summary>
+                            <div class="col-summary-frame">
+                                <div class="col-summary-frame-header">
+                                    <h4>Top Listeners</h4>
+                                </div>
+                                <div class="col-summary-frame-body">
+                                    <div v-for="(item, i) in 7" :key="i" class="col-listing __round">
+                                        <div class="_thumbnail"></div>
+                                        <div class="_title">Album Name</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </v-flex>
+                    </v-layout>
+                </v-container>
+
+                <v-container fluid>
+                    <template>
+                        <v-data-table
+                            :headers="headers"
+                            :items="desserts"
+                            class="elevation-1"
+                        >
+
+                            <template v-slot:[`item.name`]="{ item }">
+                                {{ item }}
+                                <!-- <v-img
+                                :src="item.image || require('@/assets/imgUpload.jpg')"
+                                height="50px"
+                                width="50px"
+                                style="margin: 0 auto"
+                                ></v-img> -->
+                            </template>
+                            <!-- <template v-slot:items="props">
+                            <td>{{ props.item.name }}</td>
+                            <td class="text-xs-right">{{ props.item.calories }}</td>
+                            <td class="text-xs-right">{{ props.item.fat }}</td>
+                            <td class="text-xs-right">{{ props.item.carbs }}</td>
+                            <td class="text-xs-right">{{ props.item.protein }}</td>
+                            <td class="text-xs-right">{{ props.item.iron }}</td>
+                            </template> -->
+
+                            <div>
+                                123
+                            </div>
+                        </v-data-table>
+                    </template>
+                </v-container>
+            </div>
         </v-container>
     </div>  
 </template>
