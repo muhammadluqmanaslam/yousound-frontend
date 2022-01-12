c3ec0c00ae6f8dc44789c697e7d009a371eb8e08
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2022-01-12 23:37:15 +0100

    re-enable mobile view render

--- src/main.js
+++ src/main.js
@@ -6,9 +6,7 @@ import VueIntercom from 'vue-intercom'
 import 'jquery'
 import _ from 'lodash'
 import App from './App'
-import MobileApp from './MobileApp'
 import { createRouter } from './router'
-import { createMobileRouter } from './router/mobile'
 import BrowserPage from '@/views/home/browser'
 import store from './store'
 import Vuetify from 'vuetify'
@@ -85,15 +83,6 @@ Vue.filter('timeInMinutes', Filter.timeInMinutes)
 Vue.filter('capitalize', Filter.capitalize)
 Vue.filter('truncateInMiddle', Filter.truncateInMiddle)
 
-const isMobileBrowser =
-  navigator.userAgent.match(
-    /android|blackberry|iphone|ipad|ipod|iemobile|mobile|webos/i
-  ) !== null
-// const isBot =
-//   navigator.userAgent.match(
-//     /(googlebot\/|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)/i
-//   ) !== null
-
 navigator.sayswho = (function () {
   let ua = navigator.userAgent
   let tem
@@ -143,25 +132,6 @@ if (isOldBrowser) {
   const app = new Vue({
     template: '<BrowserPage/>',
     components: { BrowserPage },
-  })
-  app.$mount('#app')
-} else if (isMobileBrowser) {
-  const router = createMobileRouter()
-  // router.beforeEach((to, frm, next) => {
-  //   if (
-  //     /^\/(protect)/.test(to.path) ||
-  //     store.state.auth.secret_code === process.env.SECRET_CODE
-  //   ) {
-  //     next()
-  //   } else {
-  //     next('/protect')
-  //   }
-  // })
-  const app = new Vue({
-    router,
-    store,
-    template: '<MobileApp/>',
-    components: { MobileApp },
   })
   app.$mount('#app')
 } else {
