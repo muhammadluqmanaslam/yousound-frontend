73145db328c29966ff35b2c605322126a26a26b3
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2022-03-12 21:28:20 +0100

    implement mobile scss

---
+++ src/components/itemTab.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="item-tab-wrapper">
+    <div class="item-tabs">
+      <slot name="itemTabs"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+.item-tab-wrapper {
+  position: relative;
+  overflow-x: scroll;
+  height: 275px;
+  width: calc(100% + 40px);
+
+  .item-tabs {
+    display: flex;
+    position: absolute;
+  }
+
+  .tab-holder {
+    width: 200px;
+    margin-right: 15px;
+  }
+}
+</style>
