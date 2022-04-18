9f757de69fc4714e98ba0fd64f0a6c22a2908ccc
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2022-04-18 14:10:04 +0100

    impl. trackcard-simple component

---
+++ src/components/trackcard-simple.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="track-card-simple">
+    <div
+      class="cover"
+      :style="`background-image: url(${coverImageURL}); height: ${height}px; width: ${width}px`"
+    ></div>
+    <div class="details">
+      <div class="title">{{ title }}</div>
+      <div class="subtitle">{{ subtitle }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+    width: {
+      type: [String, Number],
+      default: 70,
+    },
+    height: {
+      type: [String, Number],
+      default: 70,
+    },
+  },
+  computed: {
+    coverImageURL() {
+      if (this.item.cover) {
+        console.log(this.item);
+        return this.item.cover.large.url;
+      } else {
+        return "";
+      }
+    },
+    title() {
+      return this.item.name || ''
+    },
+    subtitle() {
+      return this.item.user.username || ''
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.track-card-simple {
+  display: flex;
+  align-items: center;
+
+  .cover {
+    margin-right: 15px;
+    background-size: cover;
+    background-position: center;
+  }
+  .details {
+    .title {
+      font-weight: 600;
+    }
+    .subtitle {
+      font-weight: 500;
+    }
+  }
+}
+</style>
