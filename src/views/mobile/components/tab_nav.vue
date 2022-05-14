700709231f97767bcf68663ce62f8044511c273d
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2022-05-14 15:51:42 +0100

    eslint fix

--- src/views/mobile/components/tab_nav.vue
+++ src/views/mobile/components/tab_nav.vue
@@ -1,87 +1,87 @@
 <template>
   <div class="tab-nav-wrapper">
-      <v-tabs v-model="selectedTab" :centered="centered" show-arrows grow>
-        <!-- <v-tabs-slider color="yellow"></v-tabs-slider> -->
-        <v-tabs-bar style="margin-left: 0">
+    <v-tabs v-model="selectedTab" :centered="centered" show-arrows grow>
+      <!-- <v-tabs-slider color="yellow"></v-tabs-slider> -->
+      <v-tabs-bar style="margin-left: 0">
         <v-tabs-item
-            v-for="(tab, i) in tabData"
-            :key="i"
-            :href="`#${tab.id}`"
-            active-class="activeTab"
+          v-for="(tab, i) in tabData"
+          :key="i"
+          :href="`#${tab.id}`"
+          active-class="activeTab"
         >
-            <v-chip 
-                class="_tchip text-capitalize"
-                :class="{'_rec-chip': recChip}"
-                @click="filterByCategory(tab)"
-            >
-                {{ tab.title }}
-            </v-chip>
+          <v-chip
+            class="_tchip text-capitalize"
+            :class="{ '_rec-chip': recChip }"
+            @click="filterByCategory(tab)"
+          >
+            {{ tab.title }}
+          </v-chip>
         </v-tabs-item>
-        </v-tabs-bar>
+      </v-tabs-bar>
     </v-tabs>
   </div>
 </template>
 
 <script>
 export default {
-    props: {
-        tabData: Array,
-        recChip: Boolean,
-        centered: {
-            type: Boolean,
-            default: true
-        },
+  props: {
+    tabData: Array,
+    recChip: Boolean,
+    centered: {
+      type: Boolean,
+      default: true,
     },
-    data() {
-        return {
-            selectedTab: '',
-        }
+  },
+  data() {
+    return {
+      selectedTab: "",
+    };
+  },
+  methods: {
+    filterByCategory(tab) {
+      this.$emit("tabClicked", tab);
     },
-    methods: {
-        filterByCategory(tab) {
-            this.$emit('tabClicked', tab)
-        }
-    },
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .tabs__li {
-    /deep/.tabs__item {
-        opacity: 1 !important;
-        padding-left: 0;
-        padding-right: 0;
+  /deep/.tabs__item {
+    opacity: 1 !important;
+    padding-left: 0;
+    padding-right: 0;
 
-        ._tchip {
-            font-weight: 800;
-            color: #000;
-            width: 100%;
-            display: flex;
-            justify-content: center;
-            align-content: center;
-            background-color: transparent;
-            border-radius: 90px;
-            border: 1px solid #c4c4c4;
+    ._tchip {
+      font-weight: 800;
+      color: #000;
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      align-content: center;
+      background-color: transparent;
+      border-radius: 90px;
+      border: 1px solid #c4c4c4;
 
-            &._rec-chip {
-                border-radius: 3px;
-            }
-        }
-        &.activeTab {
-            ._tchip {
-                border-radius: 90px;
-                border: 1px solid #000000;
-                background-color: #000000;
-                color: #ffffff !important;
-
-                &._rec-chip {
-                    border-radius: 3px;
-                }
-            }
-        }
-        &.tabs__item--active {
-            color: #000000;
-        }
+      &._rec-chip {
+        border-radius: 3px;
+      }
     }
+    &.activeTab {
+      ._tchip {
+        border-radius: 90px;
+        border: 1px solid #000000;
+        background-color: #000000;
+        color: #ffffff !important;
+
+        &._rec-chip {
+          border-radius: 3px;
+        }
+      }
+    }
+    &.tabs__item--active {
+      color: #000000;
+    }
+  }
 }
 </style>
