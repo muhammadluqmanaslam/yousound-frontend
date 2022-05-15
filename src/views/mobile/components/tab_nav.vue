514c2ac341bc18de0354ab717e15161f8ae367fd
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2022-05-15 01:14:44 +0100

    re-build tab nav

--- src/views/mobile/components/tab_nav.vue
+++ src/views/mobile/components/tab_nav.vue
@@ -1,24 +1,20 @@
 <template>
   <div class="tab-nav-wrapper">
-    <v-tabs v-model="selectedTab" :centered="centered" show-arrows grow>
-      <!-- <v-tabs-slider color="yellow"></v-tabs-slider> -->
-      <v-tabs-bar style="margin-left: 0">
-        <v-tabs-item
-          v-for="(tab, i) in tabData"
-          :key="i"
-          :href="`#${tab.id}`"
-          active-class="activeTab"
-        >
-          <v-chip
-            class="_tchip text-capitalize"
-            :class="{ '_rec-chip': recChip }"
-            @click="filterByCategory(tab)"
-          >
-            {{ tab.title }}
-          </v-chip>
-        </v-tabs-item>
-      </v-tabs-bar>
-    </v-tabs>
+    <div
+      v-for="(tab, i) in tabData"
+      :key="i"
+      class="tab"
+      :class="{ activeTab: tab.id == selectedTab }"
+      @click="selectedTab = tab.id"
+    >
+      <v-chip
+        class="_tchip text-capitalize"
+        :class="{ '_rec-chip': recChip }"
+        @click="filterByCategory(tab)"
+      >
+        {{ tab.title }}
+      </v-chip>
+    </div>
   </div>
 </template>
 
@@ -46,42 +42,43 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.tabs__li {
-  /deep/.tabs__item {
-    opacity: 1 !important;
-    padding-left: 0;
-    padding-right: 0;
+.tab-nav-wrapper {
+    display: flex;
+    justify-content: space-between;
 
-    ._tchip {
-      font-weight: 800;
-      color: #000;
-      width: 100%;
-      display: flex;
-      justify-content: center;
-      align-content: center;
-      background-color: transparent;
-      border-radius: 90px;
-      border: 1px solid #c4c4c4;
+    .tab {
+        width: 100%;
 
-      &._rec-chip {
-        border-radius: 3px;
-      }
-    }
-    &.activeTab {
-      ._tchip {
-        border-radius: 90px;
-        border: 1px solid #000000;
-        background-color: #000000;
-        color: #ffffff !important;
-
-        &._rec-chip {
-          border-radius: 3px;
+        &:not(:last-child) {
+            margin-right: 12px;
         }
-      }
+        ._tchip {
+            font-weight: 800;
+            color: #000000;
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            align-content: center;
+            background-color: transparent;
+            border-radius: 90px;
+            border: 1px solid #c4c4c4;
+
+            &._rec-chip {
+                border-radius: 3px;
+            }
+        }
+        &.activeTab {
+            ._tchip {
+                border-radius: 90px;
+                border: 1px solid #000000;
+                background-color: #000000;
+                color: #ffffff !important;
+
+                &._rec-chip {
+                border-radius: 3px;
+                }
+            }
+        }
     }
-    &.tabs__item--active {
-      color: #000000;
-    }
-  }
 }
 </style>
