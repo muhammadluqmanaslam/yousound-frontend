a0cb1dbc1dbd8b2b5c28e8898b5aa9ae5d336cb5
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2022-05-16 15:20:40 +0100

    implement searchTab params; eslint

--- src/components/searchInput.vue
+++ src/components/searchInput.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="search-input" :class="{isOnMobile}">
+  <div class="search-input" :class="{ isOnMobile }">
     <transition name="slide-fade">
-        <div class="global-search-box">
-        <img v-if="!isOnMobile" class="icon" src="/static/images/search.svg" width="18" />
-        <input
-            v-model="keyword"
-            class="search-field"
-            :class="{'isOnMobile-input': isOnMobile}"
-            type="search"
-            id="search"
-            :placeholder="!isOnMobile ? 'Search artist, song or keyword' : 'Search'"
-            @keyup.enter="goToSearch()"
+      <div class="global-search-box">
+        <img
+          v-if="!isOnMobile"
+          class="icon"
+          src="/static/images/search.svg"
+          width="18"
         />
-        </div>
+        <input
+          v-model="keyword"
+          class="search-field"
+          :class="{ 'isOnMobile-input': isOnMobile }"
+          type="search"
+          id="search"
+          :placeholder="placeholder"
+          @keyup.enter="goToSearch()"
+        />
+      </div>
     </transition>
   </div>
 </template>
@@ -20,26 +25,38 @@
 <script>
 export default {
   props: {
+    searchTab: {
+      type: String,
+    },
     senderRoute: {
       type: String,
+    },
+    placeholder: {
+      type: String,
+      default: "Search artist, song or keyword",
     },
     isOnMobile: Boolean,
   },
   data() {
     return {
-      keyword: '',
-    }
+      keyword: "",
+    };
   },
   methods: {
     goToSearch() {
-      const keyword = this.keyword
-      const senderRoute = this.senderRoute
+      const keyword = this.keyword;
+      const senderRoute = this.senderRoute;
+      const searchTab = this.searchTab;
 
-      this.$router.push({ name: 'Search', query: { q: keyword }, params: {senderRoute} })
+      this.$router.push({
+        name: "Search",
+        query: { q: keyword },
+        params: { senderRoute, searchTab },
+      });
     },
   },
   created() {
-    this.keyword = this.$route.query.q
+    this.keyword = this.$route.query.q;
   },
-}
+};
 </script>
