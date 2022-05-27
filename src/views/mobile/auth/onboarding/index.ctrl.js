271c23feff09d43e56791589c82b6a42eb1d46f5
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2022-05-27 22:45:21 +0100

    implement onboarding: parent

---
+++ src/views/mobile/auth/onboarding/index.ctrl.js
@@ -0,0 +1,41 @@
+import Stager from "./stager"
+import ChooseAccount from "./chooseAccount"
+import AccountType from "./accountType"
+import ProfileImage from "./profileImage"
+import AccountInfo from "./accountInfo"
+import AgeLocation from "./ageLocation"
+import GetVerified from "./getVerified"
+import CheckEmail from "./checkEmail"
+import { mapGetters, mapState } from "vuex"
+
+export default {
+  components: {
+    Stager,
+    ChooseAccount,
+    AccountType,
+    ProfileImage,
+    AccountInfo,
+    AgeLocation,
+    GetVerified,
+    CheckEmail,
+  },
+  data() {
+    return {
+      userEmail: "",
+    }
+  },
+  computed: {
+    ...mapState({
+      accountCategory: state => state.app.onboarding.accountCategory,
+      current: state => state.app.onboarding.current,
+    }),
+    ...mapGetters({
+      stages:"app/onboardingStages",
+      currentStage:"app/currentStage",
+    }),
+  },
+  methods: {},
+  updated() {
+    console.log("stage", this.current)
+  }
+}
---
+++ src/views/mobile/auth/onboarding/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="onboarding">
+    <Stager :current="current" :title="currentStage.title" />
+
+    <div v-if="accountCategory === 'creator'" class="stage-wrapper">
+      <ChooseAccount v-if="current === 1" />
+      <AccountType v-if="current === 2" />
+      <ProfileImage v-if="current === 3" />
+      <AccountInfo v-if="current === 4" />
+      <AgeLocation v-if="current === 5" />
+      <GetVerified v-if="current === 6" />
+      
+      <CheckEmail v-if="current === 7" />
+    </div>
+  </div>
+</template>
+
+<script src="./index.ctrl.js"></script>
+
+<style lang="scss" scoped>
+.onboarding {
+  position: relative;
+  min-height: 85vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  .stage-wrapper {
+    margin-top: -70px;
+  }
+}
+</style>
