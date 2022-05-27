5fb99387f9b839d5d74e37112ff2e74afc82d351
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2022-05-27 22:47:55 +0100

    implement onboarding: account type progess

---
+++ src/views/mobile/auth/onboarding/accountType.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="account-type">
+    <div class="_title">Account type</div>
+    <v-menu
+      bottom
+      left
+      offset-y
+      :nudge-top="-5"
+      :nudge-left="0"
+      class="account-type-menu"
+      content-class="account-menu__content"
+    >
+      <v-select
+        :placeholder="accountType ? accountType.title : 'Select'"
+        class="account-types-selector"
+        :class="{ 'acc-selected': accountType }"
+        single-line
+        hide-details
+        slot="activator"
+      ></v-select>
+
+      <div
+        v-for="(type, i) in accountTypes"
+        :key="i"
+        @click="selectedAccount(type)"
+        class="account-info"
+        :class="[`${type.id}-menu`]"
+      >
+        <div class="_title">{{ type.title }}</div>
+        <div class="_tags">
+          {{ type.tags }}
+        </div>
+      </div>
+    </v-menu>
+
+    <input
+      v-model="username"
+      type="text"
+      placeholder="@username"
+      class="width100 username"
+    />
+
+    <NavFooter
+      :current="current"
+      @nextStage="handleNextStage"
+      @prevStage="handlePrevStage"
+    />
+  </div>
+</template>
+
+<script>
+import { mapActions, mapMutations, mapState } from 'vuex';
+import NavFooter from "./navFooter";
+
+export default {
+  components: {
+    NavFooter
+  },
+  data() {
+    return {
+      accountType: "",
+      username: "",
+    };
+  },
+  computed: {
+    ...mapState({
+      current: state => state.app.onboarding.current,
+    }),
+    accountTypes() {
+      const accountTypes = [
+        {
+          id: "artist",
+          title: "Artist",
+          tags: "Bands, rappers, producers, podcasters + more",
+        },
+        {
+          id: "brand",
+          title: "Brand",
+          tags: "Clothing, record labels, lifestyle + more",
+        },
+      ];
+
+      return accountTypes || [];
+    },
+    validated() {
+      const { accountType, username } = this;
+      const toValidate = [accountType, username];
+
+      const isValid = toValidate.every((item) => item);
+      return isValid;
+    },
+  },
+  methods: {
+    ...mapActions({
+      gotoNextStage: "app/nextOnboardingStage",
+      gotoPrevStage: "app/prevOnboardingStage"
+    }),
+    ...mapMutations({
+      updateOnboarding: "app/updateOnboarding",
+    }),
+    selectedAccount(account) {
+      this.accountType = account;
+    },
+    handleNextStage() {
+      if (this.validated) {
+        const data = {
+          accountType: this.accountType,
+          username: this.username,
+        }
+        this.updateOnboarding(data)
+
+        this.gotoNextStage(this.current+1)
+      }
+    },
+    handlePrevStage() {
+      if (this.current !== 1) {
+        this.gotoPrevStage(this.current-1)
+      }
+    }
+  },
+  created() {
+    console.log("account type");
+  },
+};
+</script>
+
+<style lang="scss" src="../../../../../static/styles/onboarding.scss"></style>
