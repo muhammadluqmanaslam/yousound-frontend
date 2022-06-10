6d9badafd53f4e3f3d0e470a010eabb1e6ec516f
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2022-06-10 09:00:18 +0100

    implement dyanmic layout enum prop [grid, swipe]

--- src/components/itemTab.vue
+++ src/components/itemTab.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="item-tab-wrapper" :style="`min-height: ${minHeight}px;`">
-    <div class="item-tabs">
+  <div
+    class="item-tab-wrapper"
+    :class="{isSwipe, 'isGrid container fluid grid-list-lg px-0 pt-0': isGrid}"
+    :style="`min-height: ${minHeight}px;`"
+  >
+    <div
+      class="item-tabs"
+      :class="{'layout row wrap': isGrid}"
+    >
       <slot name="itemTabs"></slot>
     </div>
   </div>
@@ -13,25 +20,45 @@ export default {
       type: [String, Number],
       default: 275,
     },
+    mode: {
+      type: String,
+      default: "swipe",
+    },
+  },
+  computed: {
+    isSwipe() {
+      return this.mode === "swipe";
+    },
+    isGrid() {
+      return this.mode === "grid";
+    },
   },
 }
 </script>
 
 <style lang="scss" scoped>
 .item-tab-wrapper {
-  position: relative;
-  overflow-x: scroll;
-  // min-height: 275px;
-  width: calc(100% + 40px);
+  &.isSwipe {
+    position: relative;
+    overflow-x: scroll;
+    // min-height: 275px;
+    width: calc(100% + 40px);
 
-  .item-tabs {
-    display: inline-flex;
-    // position: absolute;
+    .item-tabs {
+      display: inline-flex;
+      // position: absolute;
+    }
+
+    .tab-holder {
+      width: 200px;
+      margin-right: 20px;
+    }
   }
 
-  .tab-holder {
-    width: 200px;
-    margin-right: 20px;
+  &.isGrid {
+    /deep/ .track-card .track-info .track-detail {
+      margin-left: 11px;
+    }
   }
 }
 </style>
