c1bbe2a63f491b45c6ab55973cd366eff188cd65
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2022-06-30 21:14:31 +0500

    comments on album page

--- src/views/album/album.ctrl.js
+++ src/views/album/album.ctrl.js
@@ -234,9 +234,9 @@ export default {
 
           this.roles = values[1].body
 
-          // if (this.currentUser) {
+          if (this.currentUser) {
             this.loadMoreComments()
-          // }
+          }
           this.$emit('updateHead')
 
           setTimeout(function () {
@@ -476,7 +476,7 @@ export default {
 
     loadMoreComments() {
       const params = {
-        commentable_type: 'Album',
+        commentable_type: 'Stream',
         commentable_id: this.album.id,
         page: this.comment_pagination.current_page + 1,
         per_page: this.comment_pagination.per_page,
