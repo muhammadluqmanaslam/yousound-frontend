44efe33f6643efd851c204d29595481dabbd02fe
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2022-07-25 12:47:11 +0500

    audio to mux

--- src/components/trackuploader.vue
+++ src/components/trackuploader.vue
@@ -148,7 +148,7 @@
 /* global $:true */
 
 import _ from 'lodash'
-
+import * as UpChunk from '@mux/upchunk'
 import TrackService from '@/services/track.js'
 import draggable from 'vuedraggable'
 import contentTopHeader from '@/components/contentTopHeader'
@@ -237,7 +237,6 @@ export default {
     pickedFile(file) {
       this.file = file
       this.filesChange(file)
-      console.log(123)
     },
     saveTrack(file) {
       TrackService.uploadTrack(file.formData)
@@ -245,6 +244,36 @@ export default {
           file.editing = false
           file.status = this.status.success
           file.track = response.body
+
+          // upload mux
+          file.track = response.body
+          console.log("track response===", response.body)
+          const upload_url = file.track.audio
+
+          const upload = UpChunk.createUpload({
+            endpoint: upload_url,
+            file: this.file[0],
+            chunkSize: 5120, // Uploads the file in ~5mb chunks
+          })
+
+          upload.on('error', (err) => {
+            this.$store.dispatch('error/showLoadingActivity', false)
+            console.error('💥', err.detail)
+          })
+
+          upload.on('progress', (progress) => {
+            this.$store.commit(
+              'error/setProgressBarValue',
+              parseInt(progress.detail)
+            )
+          })
+
+          upload.on('success', () => {
+            this.$store.dispatch('error/showLoadingActivity', false)
+            console.log("Wrap it up, we're done here. 👋")
+            // this.$router.push({ path: `/video/${this.video.id}/show` })
+          })
+
         })
         .catch((e) => {
           file.editing = false
