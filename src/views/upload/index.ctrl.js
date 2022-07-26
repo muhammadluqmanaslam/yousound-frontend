ae7964c7d61d86e0743977eac5d09563c4ed8b28
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2022-07-26 13:44:54 +0100

    implement mobile upload

--- src/views/upload/index.ctrl.js
+++ src/views/upload/index.ctrl.js
@@ -15,6 +15,9 @@ export default {
     }
   },
   computed: {
+    onMobile() {
+      return this.$vuetify.breakpoint.smAndDown;
+    },
     currentUser() {
       return this.$store.state.auth.user
     },
--- src/views/upload/index.vue
+++ src/views/upload/index.vue
@@ -1,105 +1,102 @@
 <template>
-  <div class="page upload-page mx-5">
-    <topbarNotification :content="topBarContent" ctaTitle="Connect" :cta="{ name: 'UserSettings', params: { tab: 'bank-details'}}" />
-    <h1 class="px-3 __title">Upload your content & share to the world!</h1>
+  <div class="upload-page mx-5" :class="{page: !onMobile}">
+    <div v-if="onMobile" class="allChildrenCenter flex-column text-center" style="height: 70vh">
+      <div class="mb-3">
+        To upload, manage or view sales & order history use the desktop version
+        on
+      </div>
 
-    <v-container fluid grid-list-lg>
-      <v-layout row wrap justify-start>
-        <v-flex xs12 sm6 class="upload-tab-container">
-          <div class="upload-tab-wrapper">
-            <div class="upload-tab-img uploadMusic"></div>
+      <div class="app-bold">
+        <router-link to="/" class="black--text">yousound.com</router-link>
+      </div>
+      <img :src="require('@/assets/desktop.svg')" class="mb-3" alt="desktop icon" />
+    </div>
 
-            <div class="text-left">
-              <h3>Music</h3>
-              <div class="upload-tab-description">
-                Upload your albums, connect products, assign collaborators
+    <div v-else>
+      <topbarNotification
+        :content="topBarContent"
+        ctaTitle="Connect"
+        :cta="{ name: 'UserSettings', params: { tab: 'bank-details' } }"
+      />
+      <h1 class="px-3 __title">Upload your content & share to the world!</h1>
+
+      <v-container fluid grid-list-lg>
+        <v-layout row wrap justify-start>
+          <v-flex xs12 sm6 class="upload-tab-container">
+            <div class="upload-tab-wrapper">
+              <div class="upload-tab-img uploadMusic"></div>
+
+              <div class="text-left">
+                <h3>Music</h3>
+                <div class="upload-tab-description">
+                  Upload your albums, connect products, assign collaborators
+                </div>
+                <v-btn depressed class="upload-tab-btn" :to="'/upload/album'"
+                  >Upload</v-btn
+                >
               </div>
-              <v-btn depressed class="upload-tab-btn" :to="'/upload/album'">Upload</v-btn>
             </div>
-          </div>
-        </v-flex>
+          </v-flex>
 
-        <v-flex xs12 sm6 class="upload-tab-container">
-          <div class="upload-tab-wrapper">
-            <div class="upload-tab-img uploadProduct"></div>
-            <div class="text-left">
-              <h3>Product</h3>
-              <div class="upload-tab-description">
-                Upload products, set splits & recoup costs with collaborators
+          <v-flex xs12 sm6 class="upload-tab-container">
+            <div class="upload-tab-wrapper">
+              <div class="upload-tab-img uploadProduct"></div>
+              <div class="text-left">
+                <h3>Product</h3>
+                <div class="upload-tab-description">
+                  Upload products, set splits & recoup costs with collaborators
+                </div>
+                <v-btn
+                  depressed
+                  class="upload-tab-btn"
+                  :to="{ name: 'AddProduct' }"
+                  >Upload</v-btn
+                >
               </div>
-              <v-btn depressed class="upload-tab-btn" :to="{name: 'AddProduct'}">Upload</v-btn>
             </div>
-            </div>
-        </v-flex>
+          </v-flex>
 
-        <v-flex xs12 sm6 class="upload-tab-container">
-          <div class="upload-tab-wrapper">
-            <div class="upload-tab-img uploadVideo"></div>
+          <v-flex xs12 sm6 class="upload-tab-container">
+            <div class="upload-tab-wrapper">
+              <div class="upload-tab-img uploadVideo"></div>
 
-            <div class="text-left">
-              <h3>Video</h3>
-              <div class="upload-tab-description">
-                Upload videos, connect products, albums & collaborators
+              <div class="text-left">
+                <h3>Video</h3>
+                <div class="upload-tab-description">
+                  Upload videos, connect products, albums & collaborators
+                </div>
+                <v-btn
+                  depressed
+                  class="upload-tab-btn"
+                  :to="{ name: 'VideoUpload' }"
+                  >Upload</v-btn
+                >
               </div>
-              <v-btn depressed class="upload-tab-btn" :to="{name: 'VideoUpload'}">Upload</v-btn>
             </div>
-          </div>
-        </v-flex>
+          </v-flex>
 
-        <v-flex xs12 sm6 class="upload-tab-container">
-          <div class="upload-tab-wrapper">
-            <div class="upload-tab-img uploadLive"></div>
+          <v-flex xs12 sm6 class="upload-tab-container">
+            <div class="upload-tab-wrapper">
+              <div class="upload-tab-img uploadLive"></div>
 
-            <div class="text-left">
-              <h3>Live Video</h3>
-              <div class="upload-tab-description">
-                Broadcast live RMTP, connect products, & run pay-per-view
+              <div class="text-left">
+                <h3>Live Video</h3>
+                <div class="upload-tab-description">
+                  Broadcast live RMTP, connect products, & run pay-per-view
+                </div>
+                <!-- <v-btn v-if="isOnLive" depressed class="upload-tab-btn" :to="{name: 'VideoManage'}">Broadcast Live2</v-btn> -->
+                <v-btn
+                  depressed
+                  class="upload-tab-btn"
+                  :to="{ name: broadcastLink }"
+                  >Broadcast</v-btn
+                >
               </div>
-              <!-- <v-btn v-if="isOnLive" depressed class="upload-tab-btn" :to="{name: 'VideoManage'}">Broadcast Live2</v-btn> -->
-              <v-btn depressed class="upload-tab-btn" :to="{name: broadcastLink }">Broadcast</v-btn>
             </div>
-          </div>
-        </v-flex>
-      </v-layout>
-
-      <!-- <hr class="mt-5 divider" /> -->
-    </v-container>
-
-
-    <!-- <v-container fluid grid-list-lg px-4>
-          <h1>Maximize your content</h1>
-      <v-layout row>
-        <v-flex xs6 class="meta-tab-container pl-0">
-
-          <div class="meta-tab-wrapper pl-0">
-            <div class="meta-tab-img partnerImg"></div>
-            <div class="meta-tab-description">
-              <div class="app-bold">Become YouSound Partner</div>
-               <div class="mb-3">Earn revenue from audio & video streams & user subscriptions.</div>
-
-               <router-link to="/partners">
-                 <b>Learn More</b>
-               </router-link>
-            </div>
-          </div>
-        </v-flex>
-
-        <v-flex xs6 class="meta-tab-container px-0">
-
-          <div class="meta-tab-wrapper pr-0">
-            <div class="meta-tab-img growthImg"></div>
-            <div class="meta-tab-description">
-              <div class="app-bold">Measure your growth</div>
-               <div class="mb-3">Visit your dashboard to keep track of all your analytics.</div>
-
-               <router-link to="/dashboard">
-                 <b>View Dashboard</b>
-               </router-link>
-            </div>
-          </div>
-        </v-flex>
-      </v-layout>
-    </v-container> -->
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </div>
   </div>
 </template>
 
