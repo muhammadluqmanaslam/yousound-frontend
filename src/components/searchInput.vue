01d13862b22d05dd443f74ad2a52babfcb1bceef
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2022-08-06 22:08:35 +0100

    add theme class to global search input

--- src/components/searchInput.vue
+++ src/components/searchInput.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="search-input" :class="{ isOnMobile }">
     <transition name="slide-fade">
-      <div class="global-search-box">
+      <div class="global-search-box" :class="[`is_${theme}`, {isRound}]">
         <img
           v-if="!isOnMobile"
           class="icon"
@@ -36,6 +36,14 @@ export default {
       default: "Search artist, song or keyword",
     },
     isOnMobile: Boolean,
+    theme: {
+      type: String,
+      default: "light",
+    },
+    isRound: {
+      type: Boolean,
+      default: true,
+    },
   },
   data() {
     return {
