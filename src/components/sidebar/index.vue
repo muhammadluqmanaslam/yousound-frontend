913613ad43bec61fb497808841ef20e18067525e
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2022-08-06 19:35:07 +0100

    eslint format

--- src/components/sidebar/index.vue
+++ src/components/sidebar/index.vue
@@ -1,10 +1,23 @@
 <template>
-  <v-navigation-drawer permanent app fixed dark :mini-variant="mini" mobile-break-point :width="sideBarWidth" class="sidebar">
+  <v-navigation-drawer
+    permanent
+    app
+    fixed
+    dark
+    :mini-variant="mini"
+    mobile-break-point
+    :width="sideBarWidth"
+    class="sidebar"
+  >
     <!-- <span class="white">{{ isAuthenticated }}</span> -->
     <div class="pa-3 tabs-auth-wrapper">
       <div justify-space-between align-center class="d-flex mb-3">
         <router-link :to="{ name: 'AlbumIndex' }" class="sidebar-logo">
-          <img v-if="mini" class="_mini" src="/static/images/nav_logo_white_mini.png" />
+          <img
+            v-if="mini"
+            class="_mini"
+            src="/static/images/nav_logo_white_mini.png"
+          />
           <img v-else src="/static/images/nav_logo_white.png" />
         </router-link>
 
@@ -21,19 +34,35 @@
             <img class="mr-2" src="/static/images/sign-in.svg" width="20" />
             <span v-if="!mini" class="width100">Sign In</span>
           </li>
-          <li class="cursor-pointer discover" @click="$router.push({name: 'DiscoverIndex'})">
-            <img class="mr-2 invert-color" src="/static/images/search.svg" width="20" />
+          <li
+            class="cursor-pointer discover"
+            @click="$router.push({ name: 'DiscoverIndex' })"
+          >
+            <img
+              class="mr-2 invert-color"
+              src="/static/images/search.svg"
+              width="20"
+            />
             <span v-if="!mini">Discover</span>
           </li>
         </ul>
       </div>
 
-      <div v-if="currentUser && currentUser.stream !== null && $store.state.streamPlayer == 'active'" class="live-notice" :class="{'pl-0': mini}" @click="$router.push({name: 'VideoManage'})">
-          <b v-if="!mini" class="__text">View live broadcast</b>
-          <span class="icon_wrapper" :class="{dflex: mini}">
-            <v-icon>circle</v-icon>
-            <span class="icon_text">LIVE</span>
-          </span>
+      <div
+        v-if="
+          currentUser &&
+          currentUser.stream !== null &&
+          $store.state.streamPlayer == 'active'
+        "
+        class="live-notice"
+        :class="{ 'pl-0': mini }"
+        @click="$router.push({ name: 'VideoManage' })"
+      >
+        <b v-if="!mini" class="__text">View live broadcast</b>
+        <span class="icon_wrapper" :class="{ dflex: mini }">
+          <v-icon>circle</v-icon>
+          <span class="icon_text">LIVE</span>
+        </span>
       </div>
 
       <!-- isAuthenticated: {{ isAuthenticated }} -->
@@ -45,63 +74,76 @@
           </h4>
           <div>
             <v-list-tile
-            v-for="(subMenu, ii) in parent.items"
-            :key="ii"
-            :active-class="subMenu.path ? 'activeTab' : ''"
-            class="side-tab"
-            :to="subMenu.directPath ? `/${subMenu.path}` : { name: subMenu.path }"
-            :class="[customActiveTab(subMenu) ? 'activeTab' : '', {'d-none' : subMenu.allowedUser && !subMenu.allowedUser.includes(currentUser.user_type)}]"
-          >
-            <v-list-tile-avatar>
-              <div
-                v-if="subMenu.id === 'you'"
-                class="profile-image"
-                :style="{
-                  'background-image': 'url(' + currentUser.avatar.thumb.url + ')',
-                }"
-              ></div>
-              <span v-else>
-                <img
-                  v-if="subMenu.icon.length > 50" 
-                  :src="subMenu.icon"
-                  class="icon _icon_img"
-                />
+              v-for="(subMenu, ii) in parent.items"
+              :key="ii"
+              :active-class="subMenu.path ? 'activeTab' : ''"
+              class="side-tab"
+              :to="
+                subMenu.directPath ? `/${subMenu.path}` : { name: subMenu.path }
+              "
+              :class="[
+                customActiveTab(subMenu) ? 'activeTab' : '',
+                {
+                  'd-none':
+                    subMenu.allowedUser &&
+                    !subMenu.allowedUser.includes(currentUser.user_type),
+                },
+              ]"
+            >
+              <v-list-tile-avatar>
+                <div
+                  v-if="subMenu.id === 'you'"
+                  class="profile-image"
+                  :style="{
+                    'background-image':
+                      'url(' + currentUser.avatar.thumb.url + ')',
+                  }"
+                ></div>
+                <span v-else>
+                  <img
+                    v-if="subMenu.icon.length > 50"
+                    :src="subMenu.icon"
+                    class="icon _icon_img"
+                  />
 
-                <v-icon v-else class="__icon">
-                  {{ subMenu.icon }}
-                </v-icon>
-              </span>
-            </v-list-tile-avatar>
-            <v-list-tile-title v-if="!mini" class="d-flex justify-space-between align-center">
-              <span v-if="!mini" class="__title">
-                {{ subMenu.title }}
-              </span>
-
-              <span 
-                v-if="subMenu.id === 'notifications' && badge.message > 0"
-                class="dot_notifications"
-                :class="{'mr-0 ml-1': mini}"
+                  <v-icon v-else class="__icon">
+                    {{ subMenu.icon }}
+                  </v-icon>
+                </span>
+              </v-list-tile-avatar>
+              <v-list-tile-title
+                v-if="!mini"
+                class="d-flex justify-space-between align-center"
               >
-                <v-icon size="1">circle</v-icon>
-              </span>
+                <span v-if="!mini" class="__title">
+                  {{ subMenu.title }}
+                </span>
 
-              <span 
-                v-else-if="subMenu.id === 'sales' && badge.sell > 0"
-                class="dot_notifications"
-                :class="{'mr-0 ml-1': mini}"
-              >
-                <v-icon size="1">circle</v-icon>
-              </span>
+                <span
+                  v-if="subMenu.id === 'notifications' && badge.message > 0"
+                  class="dot_notifications"
+                  :class="{ 'mr-0 ml-1': mini }"
+                >
+                  <v-icon size="1">circle</v-icon>
+                </span>
 
-              <span 
-                v-else-if="subMenu.id === 'cart' && badge.cart > 0"
-                class="dot_notifications"
-                :class="{'mr-0 ml-1': mini}"
-              >
-                <v-icon size="1">circle</v-icon>
-              </span>
-            </v-list-tile-title>
-          </v-list-tile>
+                <span
+                  v-else-if="subMenu.id === 'sales' && badge.sell > 0"
+                  class="dot_notifications"
+                  :class="{ 'mr-0 ml-1': mini }"
+                >
+                  <v-icon size="1">circle</v-icon>
+                </span>
+
+                <span
+                  v-else-if="subMenu.id === 'cart' && badge.cart > 0"
+                  class="dot_notifications"
+                  :class="{ 'mr-0 ml-1': mini }"
+                >
+                  <v-icon size="1">circle</v-icon>
+                </span>
+              </v-list-tile-title>
+            </v-list-tile>
           </div>
         </v-list>
       </template>
@@ -109,10 +151,7 @@
 
     <v-spacer></v-spacer>
 
-    <transition 
-      v-if="!onMobile"
-      name="slide-fade"
-    >
+    <transition v-if="!onMobile" name="slide-fade">
       <side-audio-player
         ref="audioPlayer"
         :isMini="mini"
@@ -121,8 +160,12 @@
     </transition>
 
     <div class="toggle-sidebar">
-      <img class="cursor-pointer" :class="{inversed: !mini}" @click.stop="mini = !mini" src="/static/images/slide-right.svg" />
-      
+      <img
+        class="cursor-pointer"
+        :class="{ inversed: !mini }"
+        @click.stop="mini = !mini"
+        src="/static/images/slide-right.svg"
+      />
     </div>
   </v-navigation-drawer>
 </template>
