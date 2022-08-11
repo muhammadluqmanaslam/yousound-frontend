9c8338b1eee35a0f15a0fc91cf862bf5265127a0
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2022-08-11 11:24:49 +0100

    repost progress

---
+++ src/views/collection/index.ctrl.js
@@ -0,0 +1,54 @@
+import StreamService from "@/services/stream";
+
+import productCard from "@/components/productcard"
+import trackCard from "@/components/trackcard"
+import contentTopHeader from "@/components/contentTopHeader"
+
+export default {
+    components: {
+        productCard,
+        trackCard,
+        contentTopHeader,
+    },
+    data() {
+        return {
+            activeTab: "music",
+            tabs: [
+                { id: "music", title: "Music" },
+                { id: "videos", title: "Videos" },
+                { id: "products", title: "Products" },
+            ],
+        }
+    },
+    computed: {
+        playlists() {
+            return this.$store.state.playlist.playlists
+        },
+    },
+    methods: {
+        onTab(tab) {
+          this.activeTab = tab
+        },
+        isActiveTab(tab) {
+          return this.activeTab === tab
+        },
+        getStream() {
+            const vid_params = {
+                genre_id: 0,
+                only_follows: false,
+                page: 1,
+                per_page: 10,
+            };
+            StreamService.getStreams(vid_params)
+                .then((response) => {
+                    this.videos = response.body.streams;
+                })
+                .catch((error) => {
+                    this.$store.dispatch("error/showErrorToast", [error]);
+                });
+        }
+    },
+    created() {
+        this.getStream()
+    },
+}
---
+++ src/views/collection/index.vue
@@ -0,0 +1,24 @@
+<template>
+  
+  <div class="page collection-page mx-5">
+
+    <content-top-header class="mt-3">
+      <template slot="topHeader">
+        <ul>
+          <li
+            v-for="tab in tabs"
+            :key="tab.id"
+            :href="`#${tab.id}`"
+            :class="{ 'active tab-active': isActiveTab(tab.id) }"
+          >
+            <label @click="onTab(tab.id)" class="innerBold">{{
+              tab.title
+            }}</label>
+          </li>
+        </ul>
+      </template>
+    </content-top-header>
+  </div>
+</template>
+
+<script type="text/javascript" src="./index.ctrl.js"></script>
