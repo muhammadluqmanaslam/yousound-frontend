ae7f926435db7bac7fffc3e7bc74d373fba77ac1
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2022-08-12 14:24:31 +0100

    add repost product, repost videos

---
+++ src/views/collection/repostProducts.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="repost-comp repost-products">Products</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/collection/repostVideos.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="repost-comp repost-videos">Videos</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
