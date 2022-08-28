bfc08604bddb32ecf2aed9181b4273eb6f099fde
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2022-08-28 23:24:05 +0100

    implement payment success and failure view

--- src/components/paymentCard/index.vue
+++ src/components/paymentCard/index.vue
@@ -1,65 +1,125 @@
 <template>
-  <transition name="slide-left">
-    <div class="payment-card">
-      <div class="dflex align-center justify-space-between">
-        <div class="payment-card-title">Payment</div>
-        <v-icon class="cancel-icon-round" @click="closePayment()">cancel</v-icon>
-      </div>
-
-      <hr />
-
-      <div class="section-wrapper">
-        <div class="card-details-wrapper left-child _child">
-          <package-details
-            :item="item"
-            :itemPrice="itemPrice"
-            :itemType="itemType"
-          />
+  <div class="payment-modal" :class="{ paymentSuccessful, paymentFailed }">
+    <transition v-if="!paymentSuccessful && !paymentFailed" name="slide-left">
+      <div class="payment-card">
+        <div class="dflex align-center justify-space-between">
+          <div class="payment-modal-title">Payment</div>
+          <v-icon class="cancel-icon-round" @click="closePayment()"
+            >cancel</v-icon
+          >
         </div>
 
-        <div class="card-details-wrapper right-child _child">
-          <card-details hidePayBtn :totalPayable="totalPayable" />
+        <hr />
 
-          <div class="region-details-wrapper">
-            <div class="region-title">Country or Region</div>
+        <div class="section-wrapper">
+          <div class="card-details-wrapper left-child _child">
+            <package-details
+              :item="item"
+              :itemPrice="itemPrice"
+              :itemType="itemType"
+            />
+          </div>
 
-            <div class="__entries">
-              <v-select
-                v-model="country"
-                hide-details
-                :items="countries"
-                item-text="country"
-                item-value="country"
-                class="pt-0"
-                placeholder="Choose Country"
-              >
-              </v-select>
-              <input
-                v-model="zipCode"
-                type="number"
-                placeholder="ZIP"
-                class="zip-code"
+          <div class="card-details-wrapper right-child _child">
+            <card-details hidePayBtn :totalPayable="totalPayable" />
+
+            <div class="region-details-wrapper">
+              <div class="region-title">Country or Region</div>
+
+              <div class="__entries">
+                <v-select
+                  v-model="country"
+                  hide-details
+                  :items="countries"
+                  item-text="country"
+                  item-value="country"
+                  class="pt-0"
+                  placeholder="Choose Country"
+                >
+                </v-select>
+                <input
+                  v-model="zipCode"
+                  type="number"
+                  placeholder="ZIP"
+                  class="zip-code"
+                />
+              </div>
+            </div>
+
+            <hr class="my-4" />
+
+            <v-btn block round dark class="pay_btn" @click="handlePayment">
+              Pay ${{ itemPrice }} /month
+            </v-btn>
+            <div class="stripe-credit black--text">
+              <span class="mr-1"> Powered By</span>
+              <img
+                :src="require('@/assets/ic_stripe-black.svg')"
+                width="40"
+                alt="stripe icon"
               />
             </div>
           </div>
-
-          <hr class="my-4" />
-
-          <v-btn block round dark class="pay_btn" @click="handlePayment">
-            Pay ${{ itemPrice }} /month
-          </v-btn>
-          <div class="stripe-credit black--text">
-            <span class="mr-1"> Powered By</span>
-            <img
-              :src="require('@/assets/ic_stripe-black.svg')"
-              width="40"
-              alt="stripe icon"
-            />
-          </div>
         </div>
       </div>
-    </div>
-  </transition>
+    </transition>
+
+    <transition v-if="paymentSuccessful" name="slide-left">
+      <div class="payment-success payment-ready">
+        <div class="payment-modal-title">Confirmation</div>
+
+        <div class="divider my-2"></div>
+
+        <div class="dflex align-center mb-3">
+          <v-icon class="success-icon result-icon">check_circle</v-icon>
+          <div class="result-text">
+            <div>Thank you</div>
+            <div>Your account is pending</div>
+          </div>
+        </div>
+
+        <div class="post">
+          We sent an email confirmation to: <br />
+          <strong>{{ userEmail }}</strong>
+        </div>
+
+        <div class="divider my-4"></div>
+
+        <package-details
+          :item="item"
+          :itemPrice="itemPrice"
+          :itemType="itemType"
+          hideTitle
+          altMeta
+          noBorder
+        />
+
+        <v-btn depressed dark round block class="mt-4"
+          >Activate your account</v-btn
+        >
+      </div>
+    </transition>
+
+    <transition v-if="paymentFailed" name="slide-left">
+      <div class="payment-success payment-ready">
+        <div class="payment-modal-title">Payment Failed</div>
+
+        <div class="divider my-2"></div>
+
+        <div class="dflex align-center mb-3">
+          <v-icon class="failed-icon result-icon">check_circle</v-icon>
+          <div class="result-text">
+            <div>Oops</div>
+            <div>Your payment was not successful</div>
+          </div>
+        </div>
+
+        <v-btn depressed dark round block class="mt-4" @click="closePayment()"
+          >Close</v-btn
+        >
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script>
@@ -82,18 +142,31 @@ export default {
     return {
       country: "",
       zipCode: "",
+      paymentSuccessful: false,
+      paymentFailed: false,
     };
   },
   components: { cardDetails, PackageDetails },
   computed: {
     ...mapState({
       countries: (state) => state.app.countries,
+      userEmail: (state) => state.app.onboarding.email,
     }),
     itemType() {
       const { list } = this.item; // update when real data is available
 
       if (list) {
         return "plan";
+      }
+    },
+    subtitleAppend() {
+      const type = this.itemType;
+
+      switch (type) {
+        case "plan":
+          return "Subscription";
+        default:
+          break;
       }
     },
     itemPrice() {
@@ -120,96 +193,130 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.payment-card {
+.payment-modal {
   position: relative;
   background-color: #ffffff;
   padding: 20px;
 
-  .payment-card-title {
+  .payment-modal-title {
     font-size: 24px;
     font-weight: bold;
   }
+  .payment-card {
+    hr {
+      height: 1px;
+      border: none;
+      width: 100%;
+      background-color: rgba(0, 0, 0, 0.1);
+      margin: 15px 0;
+    }
 
-  hr {
-    height: 1px;
-    border: none;
-    width: 100%;
-    background-color: rgba(0, 0, 0, 0.1);
-    margin: 15px 0;
+    .section-wrapper {
+      width: 100%;
+
+      display: flex;
+      justify-content: space-between;
+
+      .left-child {
+        width: 47%;
+      }
+      .right-child {
+        width: 47%;
+
+        .region-details-wrapper {
+          .region-title {
+            font-weight: 700;
+            font-size: 14px;
+            margin-bottom: 8px;
+            text-transform: capitalize;
+          }
+          .__entries {
+            box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.08),
+              0px 1px 1px rgba(0, 0, 0, 0.04);
+            border-radius: 8px;
+          }
+
+          /deep/ .input-group__input {
+            border-radius: 8px 8px 0 0;
+          }
+          .zip-code {
+            width: 100%;
+            padding: 10px;
+            border-top: 0;
+            margin-top: -3px;
+            border-radius: 0 0 8px 8px;
+
+            &::-webkit-outer-spin-button,
+            &::-webkit-inner-spin-button {
+              -webkit-appearance: none;
+              margin: 0;
+            }
+            /* Firefox */
+            &input[type="number"] {
+              -moz-appearance: textfield;
+            }
+          }
+        }
+
+        .pay_btn {
+          color: #9b9b9b !important;
+        }
+
+        .stripe-credit {
+          text-align: center;
+          font-weight: bold;
+          font-size: 12px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          margin-top: 15px;
+        }
+      }
+    }
   }
 
-  .section-wrapper {
-    width: 100%;
+  .payment-ready {
+    .result-icon {
+      font-size: 60px;
+      margin-right: 10px;
 
-    display: flex;
-    justify-content: space-between;
-
-    .left-child {
-      width: 47%;
+      &.success-icon {
+        color: #3caf47;
+      }
+      &.failed-icon {
+        color: red;
+      }
     }
-    .right-child {
-      width: 47%;
+    .result-text {
+      font-size: 16px;
+      font-weight: bold;
+    }
 
-      .region-details-wrapper {
-        .region-title {
-          font-weight: 700;
-          font-size: 14px;
-          margin-bottom: 8px;
-          text-transform: capitalize;
-        }
-        .__entries {
-          box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.08),
-            0px 1px 1px rgba(0, 0, 0, 0.04);
-          border-radius: 8px;
-        }
-
-        /deep/ .input-group__input {
-          border-radius: 8px 8px 0 0;
-        }
-        .zip-code {
-          width: 100%;
-          padding: 10px;
-          border-top: 0;
-          margin-top: -3px;
-          border-radius: 0 0 8px 8px;
-
-          &::-webkit-outer-spin-button,
-          &::-webkit-inner-spin-button {
-            -webkit-appearance: none;
-            margin: 0;
-          }
-          /* Firefox */
-          &input[type="number"] {
-            -moz-appearance: textfield;
-          }
-        }
-      }
-
-      .pay_btn {
-        color: #9b9b9b !important;
-      }
-
-      .stripe-credit {
-        text-align: center;
-        font-weight: bold;
-        font-size: 12px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        margin-top: 15px;
-      }
+    .divider {
+      width: 100%;
+      border-bottom: 1px solid #ccb4b400;
     }
   }
 }
 </style>
 
-<style>
+<style lang="scss">
 .dialog.payment-dialog {
-  width: 800px;
+  width: auto;
   border-radius: 20px;
   background-color: rgba(0, 0, 0, 0.9);
+
+  .payment-modal {
+    width: 755px;
+
+    &.paymentSuccessful,
+    &.paymentFailed {
+      width: 398px;
+    }
+  }
 }
 .overlay.overlay--active {
+  width: auto;
   background-color: rgba(0, 0, 0, 0.9);
 }
 </style>
