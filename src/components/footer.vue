5b212f1e08eba7de23f805bbf1af24aed213205d
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2022-09-15 18:02:08 +0100

    introduce hideDownload prop

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -4,8 +4,16 @@
     <!-- <a href="//yousound.ghost.io" target="_blank">Blog</a> -->
     <router-link to="/terms">Terms</router-link>
     <router-link to="#">Privacy</router-link>
-    <router-link to="#">Download App</router-link>
+    <router-link v-if="!hideDownload" to="#">Download App</router-link>
     <!-- <router-link to="#">Contact</router-link> -->
     <div class="copyright">© {{ new Date().getFullYear() }} YouSound, Inc.</div>
   </v-footer>
 </template>
+
+<script>
+export default {
+  props: {
+    hideDownload: Boolean,
+  },
+}
+</script>
