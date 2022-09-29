2bf749a50299efaceaef3a0ca2e37a3312d49772
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2022-09-29 11:51:55 +0500

    Upload video duration time reduce

--- src/views/video/upload.ctrl.js
+++ src/views/video/upload.ctrl.js
@@ -287,7 +287,7 @@ export default {
         if (response.status == 200) {
           const duration = video.duration/60
           const plan = this.currentUser.plan
-          const video_duration_limit = plan === "pro" ? 180 : 120
+          const video_duration_limit = plan === "pro" ? 120 : 60
 
           if (duration > video_duration_limit) {
             this.videoFile = null;
