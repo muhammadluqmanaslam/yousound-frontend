187e8dcbc1472c64e0ab4735bc2f1c91b62fd7fa
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2022-10-14 19:02:58 +0500

    Subscribe changes

--- src/views/mobile/auth/learnMore.vue
+++ src/views/mobile/auth/learnMore.vue
@@ -200,7 +200,7 @@
       </transition>
 
       <transition name="fade">
-        <div v-if="(activeTab === 'creator') || onMobile" class="learn-more-body _creators" :class="{'mt-5': onMobile, onMobile}">
+        <div v-if="(activeTab === 'creator') || onMobile" class="learn-more-body _creators mt-extra" :class="{'mt-5': onMobile, onMobile}">
           <div v-if="onMobile" class="intro-section">
             <h1 class="intro-title">For Creators</h1>
             <hr class="light intro-divider">
@@ -228,7 +228,7 @@
               </v-flex>
             </v-layout>
 
-            <v-layout row wrap align-center justify-space-between layer-layout>
+            <v-layout row wrap align-center justify-space-between my-6 layer-layout>
               <v-flex xs12 sm6>
                 <div class="tile-context">
                   <h2 class="_title">Keep 100% of sales</h2>
@@ -272,7 +272,7 @@
               </v-flex>
             </v-layout>
 
-            <v-layout row wrap align-center justify-space-between layer-layout>
+            <v-layout row wrap align-center justify-space-between my-6 layer-layout>
               <v-flex xs12 sm6>
                 <div class="tile-context">
                   <h2 class="_title">Connect with text</h2>
@@ -316,7 +316,7 @@
               </v-flex>
             </v-layout>
 
-            <v-layout row wrap align-center justify-space-between layer-layout>
+            <v-layout row wrap align-center justify-space-between my-6 layer-layout>
               <v-flex xs12 sm6>
                 <div class="tile-context">
                   <h2 class="_title">Advanced analytics</h2>
@@ -414,7 +414,10 @@ export default {
   padding: 0 25px 40px 25px;
 
   .my-6{
-    margin: 50px 0px;
+    margin: 50px 0px !important;
+  }
+  .mt-extra{
+    margin-top: 125px !important;
   }
 
 .intro-title {
@@ -620,6 +623,7 @@ export default {
       ._img {
         width: 100% !important;
         height: 258px !important;
+        box-shadow: 0px 10px 50px rgba(34, 34, 34, 0.15);
       }
 
       &::before {
