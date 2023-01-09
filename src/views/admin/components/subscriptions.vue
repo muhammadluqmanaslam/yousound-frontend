6f5b2684edf32a1ea7ece47f595f6104673c3ad5
Muhammad Luqman Aslam <muhammadluqmanaslam1@gmail.com>
2023-01-09 17:43:33 +0500

    admin -> subscriptions updated

--- src/views/admin/components/subscriptions.vue
+++ src/views/admin/components/subscriptions.vue
@@ -72,21 +72,19 @@
                   {{ props.item.initial_signup_type }}
                 </td>
                 <td class="text-xs-center">
-                  {{ props.item.first_name }}
+                  {{ props.item.first_name }} {{ props.item.last_name }}
                 </td>
-                <td class="text-xs-center">
-                  {{ props.item.last_name }}
-                </td>
-                <td class="text-xs-center">
+                
+                <td  v-if="active_tab != 'cancelled'"  class="text-xs-center">
                   {{ props.item.trial_start | formatDate }}
                 </td>
-                <td class="text-xs-center">
+                <td  v-if="active_tab != 'cancelled'"  class="text-xs-center">
                   {{ props.item.plan }}
                 </td>
                 <td class="text-xs-center">
                   {{ props.item.email }}
                 </td>
-                <td class="text-xs-center">
+                <td  v-if="active_tab != 'cancelled'" class="text-xs-center">
                   {{ getNumberOfMonths(props.item.created_at) }}
                 </td>
                 <td v-if="active_tab == 'trial'" class="text-xs-center">
@@ -113,7 +111,7 @@
               :rows-per-page-items="per_page_options"
               :total-items="total_subscriptions.length"
               :search="search"
-              class="user-table-x"
+              class="user-table-x text-xs-left"
             >
               <template slot="items" slot-scope="props">
                 <td class="text-xs-left">
@@ -132,16 +130,17 @@
                   {{ props.item.user_type | capitalize }}
                 </td>
                 <td class="text-xs-center">
-                  {{ props.item.first_name }}
+                  {{ props.item.first_name }} {{ props.item.last_name }}
                 </td>
                 <td class="text-xs-center">
-                  {{ props.item.last_name }}
-                </td>
-                <td class="text-xs-center">
-                  {{  Math.round(Math.abs((new Date(props.item.trial_end) - new Date()) / 86400000)) }}
+                  {{ props.item.created_at | formatDate }}
+                  <!-- {{  Math.round(Math.abs((new Date(props.item.trial_end) - new Date()) / 86400000)) }} -->
                 </td>
                 <td class="text-xs-center">
                   {{ props.item.plan }}
+                </td>
+                <td class="text-xs-center">
+                  {{  Math.round(Math.abs((new Date(props.item.trial_end) - new Date()) / 86400000)) }} {{ props.item.trial_end ? 'days' : '' }}
                 </td>
                 <td class="text-xs-center">
                   {{ props.item.email }}
@@ -149,9 +148,9 @@
                 <td class="text-xs-center">
                   {{ getNumberOfMonths(props.item.created_at) }}
                 </td>
-                <td class="text-xs-center">
+                <!-- <td class="text-xs-center">
                   {{ props.item.trial_end | formatDate }}
-                </td>
+                </td> -->
                 <td class="text-xs-center width-200">
                   <div class="dflex align-center">
                     <v-select attach
